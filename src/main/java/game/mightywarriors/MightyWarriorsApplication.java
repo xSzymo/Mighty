4ea7620781d2557071dd40a5405517288056804f
xSzymo <szymekssss@wp.pl>
2017-10-16 22:29:14 +0200

    Initial commit

---
+++ src/main/java/game/mightywarriors/MightyWarriorsApplication.java
@@ -0,0 +1,12 @@
+package game.mightywarriors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MightyWarriorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MightyWarriorsApplication.class, args);
+	}
+}
