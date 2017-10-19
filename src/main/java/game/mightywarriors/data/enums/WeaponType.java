459486a53dad58fb21abe1150c5e7dd5d2e6e62f
xSzymo <szymekssss@wp.pl>
2017-10-19 20:06:44 +0200

    Created enum weaponType

---
+++ src/main/java/game/mightywarriors/data/enums/WeaponType.java
@@ -0,0 +1,19 @@
+package game.mightywarriors.data.enums;
+
+public enum WeaponType {
+    WEAPON("weapon"), OFFHAND("offhand"),
+    HELMET("helmet"), ARMOR("armor"),
+    GLOVES("gloves"), LEGS("legs"),
+    BOOTS("boots"), RING("ring"),
+    BRACELET("bracelet"), NECKLACE("necklace");
+
+    private String type;
+
+    WeaponType(String type) {
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
+}
