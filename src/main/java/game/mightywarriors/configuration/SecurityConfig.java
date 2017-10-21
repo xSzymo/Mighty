170f432447678ee56bd4303fff802358ccd0142f
xSzymo <szymekssss@wp.pl>
2017-10-21 09:32:36 +0200

    Changed configuration for temporary API

--- src/main/java/game/mightywarriors/configuration/SecurityConfig.java
+++ src/main/java/game/mightywarriors/configuration/SecurityConfig.java
@@ -19,51 +19,84 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
-    private BCryptPasswordEncoder bCryptPasswordEncoder;
-
-    @Autowired
-    private DataSource dataSource;
-
-    private String usersQuery = "select email, password, active from user where email=?";
-    private String rolesQuery = "select u.email, r.role from user u inner join user_role ur on(u.user_id=ur.user_id) inner join role r on(ur.role_id=r.role_id) where u.email=?\n";
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth)
-            throws Exception {
-        auth.
-                jdbcAuthentication()
-                .usersByUsernameQuery(usersQuery)
-                .authoritiesByUsernameQuery(rolesQuery)
-                .dataSource(dataSource)
-                .passwordEncoder(bCryptPasswordEncoder);
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("admin").password("admin").roles("USER");
     }
 
+    //.csrf() is optional, enabled by default, if using WebSecurityConfigurerAdapter constructor
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
-        http.
-                authorizeRequests()
-                .antMatchers("/").permitAll()
+        http.authorizeRequests()
                 .antMatchers("/login").permitAll()
-                .antMatchers("/registration").permitAll()
-                .antMatchers("/hello").permitAll()
-                .antMatchers("/admin/**").hasAuthority("ADMIN").anyRequest()
-                .authenticated().and().csrf().disable().formLogin()
-                .loginPage("/login").failureUrl("/login?error=true")
-                .defaultSuccessUrl("/admin/home")
-                .usernameParameter("email")
-                .passwordParameter("password")
-                .and().logout()
-                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                .logoutSuccessUrl("/").and().exceptionHandling()
-                .accessDeniedPage("/access-denied");
+                .anyRequest().permitAll()
+                .and().csrf().disable()
+                .formLogin().loginPage("/login").failureUrl("/login?error")
+                .usernameParameter("username").passwordParameter("password").successForwardUrl("/hello")
+
+                .and()
+                .logout().logoutSuccessUrl("/login?logout");
+
     }
 
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        web
-                .ignoring()
-                .antMatchers("/resources/**", "/static/**", "/css/**", "/js/**", "/images/**");
-    }
-
+//    @Autowired
+//    private BCryptPasswordEncoder bCryptPasswordEncoder;
+//
+//    @Autowired
+//    private DataSource dataSource;
+//
+//    private String usersQuery = "select login, password, active from user where login=?";
+//    private String rolesQuery = "select u.email, r.role from user u inner join user_role ur on(u.user_id=ur.user_id) inner join role r on(ur.role_id=r.role_id) where u.email=?\n";
+//
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth)
+//            throws Exception {
+//        auth.inMemoryAuthentication().withUser("admin").password("admin").roles("admin");
+////        auth.
+////                jdbcAuthentication()
+////                .usersByUsernameQuery(usersQuery)
+//////                .authoritiesByUsernameQuery(rolesQuery)
+////                .dataSource(dataSource)
+////                .passwordEncoder(bCryptPasswordEncoder);
+//    }
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//
+//        http.authorizeRequests()
+//                .antMatchers("/", "/home").permitAll()
+//                .antMatchers("/login").permitAll()
+//                .antMatchers("/admin/**").access("hasRole('ADMIN')")
+//                .antMatchers("/db/**").access("hasRole('ADMIN') and hasRole('DBA')")
+//                .and().formLogin().loginPage("/login")
+//                .usernameParameter("user").passwordParameter("password")
+//                .and().csrf()
+//                .and().exceptionHandling().accessDeniedPage("/Access_Denied");
+//
+//
+////        http.
+////                authorizeRequests()
+////                .antMatchers("/").permitAll()
+////                .antMatchers("/login").permitAll()
+////                .antMatchers("/registration").permitAll()
+////                .antMatchers("/user").permitAll()
+////                .antMatchers("/user/{id}").permitAll()
+////                .antMatchers("/admin/**").hasAuthority("ADMIN").anyRequest()
+////                .authenticated().and().csrf().disable()
+////
+////                .formLogin().loginPage("/logincustom").loginProcessingUrl("/doLogin")
+////                .usernameParameter("ssoId").passwordParameter("password");
+////
+//////                .logout()
+//////                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+//////                .logoutSuccessUrl("/").and().exceptionHandling()
+//////                .accessDeniedPage("/access-denied");
+//    }
+//
+//    @Override
+//    public void configure(WebSecurity web) throws Exception {
+//        web
+//                .ignoring()
+//                .antMatchers("/resources/**", "/static/**", "/css/**", "/js/**", "/images/**");
+//    }
 }
