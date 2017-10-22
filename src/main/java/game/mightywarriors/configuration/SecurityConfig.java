7396a6be6053cf35ad5ec4b754a17e7616810a86
xSzymo <szymekssss@wp.pl>
2017-10-23 00:41:53 +0200

    Enable cross

--- src/main/java/game/mightywarriors/configuration/SecurityConfig.java
+++ src/main/java/game/mightywarriors/configuration/SecurityConfig.java
@@ -23,12 +23,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
-        http.csrf().disable()
+        http.cors().and().csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/", "/home", "/about").permitAll()
-                .antMatchers("/user/**").hasAnyRole("admin")
                 .antMatchers("/hello").permitAll()
-                .anyRequest().authenticated()
+                .anyRequest().permitAll()
                 .and()
                 .formLogin()
                 .loginPage("/login")
--- src/main/java/game/mightywarriors/configuration/WebMvcConfig.java
+++ src/main/java/game/mightywarriors/configuration/WebMvcConfig.java
@@ -3,6 +3,8 @@ package game.mightywarriors.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
@@ -12,6 +14,16 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
     public BCryptPasswordEncoder passwordEncoder() {
         BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
         return bCryptPasswordEncoder;
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
     }
 
 }
