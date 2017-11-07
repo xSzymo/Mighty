82fbc229358633bd9c88abc821f17a3f099a23c0
xSzymo <szymekssss@wp.pl>
2017-11-07 21:07:58 +0100

    CES: 1 - add userRole service & test

--- src/main/java/game/mightywarriors/data/services/UserRoleService.java
+++ src/main/java/game/mightywarriors/data/services/UserRoleService.java
@@ -1,10 +1,71 @@
 package game.mightywarriors.data.services;
 
+import game.mightywarriors.data.repositories.UserRoleRepository;
+import game.mightywarriors.data.tables.UserRole;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.Collection;
+import java.util.LinkedList;
 
 @Service
 @Transactional
 public class UserRoleService {
+    @Autowired
+    private UserRoleRepository repository;
+
+    public void save(UserRole image) {
+        if (image != null)
+            repository.save(image);
+    }
+
+    public void save(Collection<UserRole> images) {
+        images.forEach(
+                x -> {
+                    if (x != null)
+                        repository.save(x);
+                });
+    }
+
+    public UserRole findOne(long id) {
+        try {
+            return repository.findById(id);
+        } catch (NullPointerException e) {
+            return null;
+        }
+    }
+
+    public UserRole findOne(UserRole image) {
+        try {
+            return findOne(image.getId());
+        } catch (NullPointerException e) {
+            return null;
+        }
+    }
+
+    public LinkedList<UserRole> findAll() {
+        return repository.findAll();
+    }
+
+    public void delete(long id) {
+        if (repository.findById(id) != null)
+            repository.deleteById(id);
+    }
+
+    public void delete(UserRole image) {
+        try {
+            delete(image.getId());
+        } catch (NullPointerException e) {
+
+        }
+    }
+
+    public void delete(Collection<UserRole> addresses) {
+        addresses.forEach(
+                x -> {
+                    if (x != null)
+                        delete(x);
+                });
+    }
 }
---
+++ src/test/java/game/mightywarriors/data/services/UserRoleServiceTest.java
@@ -0,0 +1,126 @@
+package game.mightywarriors.data.services;
+
+import game.mightywarriors.data.tables.UserRole;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.LinkedList;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UserRoleServiceTest {
+    @Autowired
+    private UserRoleService objectUnderTest;
+
+    private LinkedList<UserRole> userRoles;
+
+    @Before
+    public void beforeEachTest() {
+        userRoles = new LinkedList<>();
+        userRoles.add(new UserRole("admin"));
+        userRoles.add(new UserRole("user"));
+        userRoles.add(new UserRole("other"));
+    }
+
+    @After
+    public void afterEachTest() {
+        userRoles.forEach(objectUnderTest::delete);
+    }
+
+    @Test
+    public void save() throws Exception {
+        objectUnderTest.save(userRoles.getFirst());
+
+        long counter = objectUnderTest.findAll().stream().count();
+
+        assertEquals(1, counter);
+    }
+
+    @Test
+    public void saveCollection() throws Exception {
+        objectUnderTest.save(userRoles);
+
+        long counter = objectUnderTest.findAll().stream().count();
+
+        assertEquals(3, counter);
+    }
+
+    @Test
+    public void findOne() throws Exception {
+        objectUnderTest.save(userRoles.getFirst());
+
+        assertNotNull(objectUnderTest.findOne(userRoles.getFirst()));
+    }
+
+    @Test
+    public void findOne1() throws Exception {
+        objectUnderTest.save(userRoles.getFirst());
+
+        assertNotNull(objectUnderTest.findOne(userRoles.getFirst().getId()));
+    }
+
+    @Test
+    public void findAll() throws Exception {
+        objectUnderTest.save(userRoles);
+
+        long counter = objectUnderTest.findAll().stream().count();
+
+        assertEquals(3, counter);
+    }
+
+    @Test
+    public void delete() throws Exception {
+        objectUnderTest.save(userRoles.getFirst());
+
+        long counter = objectUnderTest.findAll().stream().count();
+
+        assertEquals(1, counter);
+
+        objectUnderTest.delete(userRoles.getFirst());
+
+        counter = objectUnderTest.findAll().stream().count();
+
+        assertEquals(0, counter);
+        userRoles.clear();
+    }
+
+    @Test
+    public void delete1() throws Exception {
+        objectUnderTest.save(userRoles.getFirst());
+
+        long counter = objectUnderTest.findAll().stream().count();
+
+        assertEquals(1, counter);
+
+        objectUnderTest.delete(userRoles.getFirst().getId());
+
+        counter = objectUnderTest.findAll().stream().count();
+
+        assertEquals(0, counter);
+        userRoles.clear();
+    }
+
+    @Test
+    public void delete2() throws Exception {
+        objectUnderTest.save(userRoles);
+
+        long counter = objectUnderTest.findAll().stream().count();
+
+        assertEquals(3, counter);
+
+        objectUnderTest.delete(userRoles);
+
+        counter = objectUnderTest.findAll().stream().count();
+
+        assertEquals(0, counter);
+        userRoles.clear();
+    }
+}
