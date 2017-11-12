560504fede2b327f5f6efc811444062d29bdaf39
xSzymo <szymekssss@wp.pl>
2017-11-12 23:34:14 +0100

    add 2 controllers for showing data

---
+++ src/main/java/game/mightywarriors/web/rest/api/MissionsApiController.java
@@ -0,0 +1,29 @@
+package game.mightywarriors.web.rest.api;
+
+
+import game.mightywarriors.data.repositories.ChampionRepository;
+import game.mightywarriors.data.services.MissionService;
+import game.mightywarriors.data.tables.Champion;
+import game.mightywarriors.data.tables.Mission;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.LinkedList;
+
+@RestController
+public class MissionsApiController {
+    @Autowired
+    MissionService missionService;
+
+    @GetMapping("missions")
+    public LinkedList<Mission> getChampions() {
+        return missionService.findAll();
+    }
+
+    @GetMapping("missions/{id}")
+    public Mission getChampion(@PathVariable("id") String id) {
+        return missionService.findOne(Long.parseLong(id));
+    }
+}
---
+++ src/main/java/game/mightywarriors/web/rest/api/MonstersApiController.java
@@ -0,0 +1,29 @@
+package game.mightywarriors.web.rest.api;
+
+
+import game.mightywarriors.data.services.MissionService;
+import game.mightywarriors.data.services.MonsterService;
+import game.mightywarriors.data.tables.Mission;
+import game.mightywarriors.data.tables.Monster;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.LinkedList;
+
+@RestController
+public class MonstersApiController {
+    @Autowired
+    MonsterService monsterService;
+
+    @GetMapping("monsters")
+    public LinkedList<Monster> getChampions() {
+        return monsterService.findAll();
+    }
+
+    @GetMapping("monsters/{id}")
+    public Monster getChampion(@PathVariable("id") String id) {
+        return monsterService.findOne(Long.parseLong(id));
+    }
+}
