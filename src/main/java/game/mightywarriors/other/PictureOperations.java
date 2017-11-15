fc8dc5dcd0fe1e0b0653f2436f0747ad3ae73793
xSzymo <szymekssss@wp.pl>
2017-11-15 10:25:53 +0100

    T: 0 - delete unused classes

--- src/main/java/game/mightywarriors/other/PictureOperations.java
+++
@@ -1,9 +0,0 @@
-package game.mightywarriors.other;
-
-import game.mightywarriors.data.tables.Image;
-
-public class PictureOperations {
-    public void deletePicture(Image image) {
-        //delete operation on firebase
-    }
-}
