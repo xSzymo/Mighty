2ef40b5691044fd43ca427c50f5a13de182815d3
xSzymo <szymekssss@wp.pl>
2017-11-25 16:53:18 +0100

    T: 12 - create roundFightPerformer

---
+++ src/main/java/game/mightywarriors/helpers/fight/RoundFightPerformer.java
@@ -0,0 +1,45 @@
+package game.mightywarriors.helpers.fight;
+
+import game.mightywarriors.other.jsonObjects.fights.ChampionModel;
+import game.mightywarriors.other.jsonObjects.fights.RoundProcess;
+import org.springframework.stereotype.Service;
+
+import java.util.LinkedList;
+
+@Service
+public class RoundFightPerformer {
+    public RoundProcess performSingleRound(RoundProcess round, int userTurn, int opponentTurn, boolean isUserTurn) {
+        LinkedList<ChampionModel> userChampions = round.getUserChampions();
+        LinkedList<ChampionModel> opponentChampions = round.getOpponentChampions();
+
+        if (isUserTurn)
+            round.setOpponentChampions(championAttack(opponentChampions, userChampions.get(userTurn)));
+        else
+            round.setUserChampions(championAttack(userChampions, opponentChampions.get(opponentTurn)));
+
+        return round;
+    }
+
+    private LinkedList<ChampionModel> championAttack(LinkedList<ChampionModel> champions, ChampionModel championModel) {
+        for (ChampionModel champion : champions) {
+            double hp = new Double(champion.getHp());
+
+            if (hp <= 0)
+                continue;
+
+            if ((Math.random() * (100 - 0)) + 0 <= champion.getCriticChance()) {
+                hp -= (championModel.getStrength() / champion.getArmor()) * 2;
+                hp -= (championModel.getIntelligence() / champion.getMagicResist()) * 2;
+            } else {
+                hp -= (championModel.getStrength() / champion.getArmor());
+                hp -= (championModel.getIntelligence() / champion.getMagicResist());
+            }
+
+            championModel.setDmg(champion.getHp() - hp);
+            champion.setHp(hp);
+
+            return champions;
+        }
+        return null;
+    }
+}
