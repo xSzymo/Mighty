89482d54c0c94ad370c906b4df6bf2d779a3e393
xSzymo <szymekssss@wp.pl>
2017-11-25 16:13:35 +0100

    T: 12 - add FightResult & RoundProcess

---
+++ src/main/java/game/mightywarriors/other/jsonObjects/fights/RoundProcess.java
@@ -0,0 +1,36 @@
+package game.mightywarriors.other.jsonObjects.fights;
+
+import java.util.Comparator;
+import java.util.LinkedList;
+
+public class RoundProcess {
+    private int roundNr;
+    private LinkedList<ChampionModel> userChampions;
+    private LinkedList<ChampionModel> opponentChampions;
+
+    public int getRoundNr() {
+        return roundNr;
+    }
+
+    public void setRoundNr(int roundNr) {
+        this.roundNr = roundNr;
+    }
+
+    public LinkedList<ChampionModel> getUserChampions() {
+        return new LinkedList<>(userChampions);
+    }
+
+    public void setUserChampions(LinkedList<ChampionModel> userChampions) {
+        this.userChampions = userChampions;
+        userChampions.sort(Comparator.comparing(ChampionModel::getLevel));
+    }
+
+    public LinkedList<ChampionModel> getOpponentChampions() {
+        return new LinkedList<>(opponentChampions);
+    }
+
+    public void setOpponentChampions(LinkedList<ChampionModel> opponentChampions) {
+        this.opponentChampions = opponentChampions;
+        opponentChampions.sort(Comparator.comparing(ChampionModel::getLevel));
+    }
+}
