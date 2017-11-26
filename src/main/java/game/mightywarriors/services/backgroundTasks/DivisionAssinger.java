40137d6de9b861a6e7838d55b0e69643acee6bfc
xSzymo <szymekssss@wp.pl>
2017-11-26 18:44:39 +0100

    T: 10 - create division system in service DivisionAssigner created simple test for it

---
+++ src/main/java/game/mightywarriors/services/backgroundTasks/DivisionAssinger.java
@@ -0,0 +1,98 @@
+package game.mightywarriors.services.backgroundTasks;
+
+import game.mightywarriors.configuration.system.SystemVariablesManager;
+import game.mightywarriors.data.services.DivisionService;
+import game.mightywarriors.data.services.UserService;
+import game.mightywarriors.data.tables.Division;
+import game.mightywarriors.data.tables.User;
+import game.mightywarriors.helpers.comparators.UsersDivisionComparator;
+import game.mightywarriors.helpers.counters.StatisticCounter;
+import game.mightywarriors.other.enums.League;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.LinkedList;
+
+@Service
+public class DivisionAssinger {
+    @Autowired
+    private DivisionService divisionService;
+    @Autowired
+    private UserService userService;
+
+    private final StatisticCounter statisticCounter = new StatisticCounter();
+
+    @Transactional
+    public void assignUsersDivisions() {
+        LinkedList<User> users = userService.findAll();
+        users.sort(new UsersDivisionComparator());
+
+        Division challenger = divisionService.findByLeague(League.CHALLENGER);
+        Division diamond = divisionService.findByLeague(League.DIAMOND);
+        Division gold = divisionService.findByLeague(League.GOLD);
+        Division silver = divisionService.findByLeague(League.SILVER);
+        Division bronze = divisionService.findByLeague(League.BRONZE);
+        Division wood = divisionService.findByLeague(League.WOOD);
+
+        challenger.clearUsers();
+        diamond.clearUsers();
+        gold.clearUsers();
+        silver.clearUsers();
+        bronze.clearUsers();
+        wood.clearUsers();
+
+
+        for (User user : users) {
+            long level = user.getUserChampiongHighestLevel();
+
+            if (level < 30)
+                continue;
+            double points = statisticCounter.countUserStatistic(user);
+
+
+            if (challenger.getUsers().size() < SystemVariablesManager.MAX_PLAYERS_IN_CHALLENGER)
+                if (level >= SystemVariablesManager.MIN_LEVEL_FOR_CHALLENGER && points > SystemVariablesManager.MIN_POINTS_FOR_CHALLENGER) {
+                    challenger.getUsers().add(user);
+                    continue;
+                }
+
+            if (diamond.getUsers().size() < SystemVariablesManager.MAX_PLAYERS_IN_DIAMOND)
+                if (level >= SystemVariablesManager.MIN_LEVEL_FOR_DIAMOND && points >= SystemVariablesManager.MIN_POINTS_FOR_DIAMOND) {
+                    diamond.getUsers().add(user);
+                    continue;
+                }
+
+            if (gold.getUsers().size() < (long) users.size() * (SystemVariablesManager.MAX_PERCENT_PLAYERS_IN_GOLD * 0.1))
+                if (level >= SystemVariablesManager.MIN_LEVEL_FOR_GOLD && points > SystemVariablesManager.MIN_POINTS_FOR_GOLD) {
+                    gold.getUsers().add(user);
+                    continue;
+                }
+
+            if (silver.getUsers().size() < (long) users.size() * (SystemVariablesManager.MAX_PERCENT_PLAYERS_IN_SILVER * 0.1))
+                if (level >= SystemVariablesManager.MIN_LEVEL_FOR_SILVER && points > SystemVariablesManager.MIN_POINTS_FOR_SILVER) {
+                    silver.getUsers().add(user);
+                    continue;
+                }
+
+            if (bronze.getUsers().size() < (long) users.size() * (SystemVariablesManager.MAX_PERCENT_PLAYERS_IN_BRONZE * 0.1))
+                if (level >= SystemVariablesManager.MIN_LEVEL_FOR_BRONZE && points > SystemVariablesManager.MIN_POINTS_FOR_BRONZE) {
+                    bronze.getUsers().add(user);
+                    continue;
+                }
+
+            if (wood.getUsers().size() < (long) users.size() * (SystemVariablesManager.MAX_PERCENT_PLAYERS_IN_WOOD * 0.1))
+                if (level >= SystemVariablesManager.MIN_LEVEL_FOR_WOOD && points > SystemVariablesManager.MIN_POINTS_FOR_WOOD) {
+                    wood.getUsers().add(user);
+                    continue;
+                }
+        }
+
+        divisionService.save(challenger);
+        divisionService.save(diamond);
+        divisionService.save(gold);
+        divisionService.save(silver);
+        divisionService.save(bronze);
+        divisionService.save(wood);
+    }
+}
---
+++ src/test/java/integration/game/mightywarriors/services/backgroundTasks/DivisionAssingerTest.java
@@ -0,0 +1,101 @@
+package integration.game.mightywarriors.services.backgroundTasks;
+
+import game.mightywarriors.data.services.DivisionService;
+import game.mightywarriors.data.services.UserService;
+import game.mightywarriors.data.tables.*;
+import game.mightywarriors.other.enums.League;
+import game.mightywarriors.other.enums.WeaponType;
+import game.mightywarriors.services.backgroundTasks.DivisionAssinger;
+import integration.game.mightywarriors.config.IntegrationTestsConfig;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.junit.Assert.assertEquals;
+
+public class DivisionAssingerTest extends IntegrationTestsConfig {
+    @Autowired
+    private DivisionAssinger divisionAssinger;
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private DivisionService divisionService;
+
+    @Before
+    public void setUp() throws Exception {
+        userService.save(setUpUser1());
+        userService.save(setUpUser2());
+        userService.save(setUpUser3());
+    }
+
+    @Test
+    public void assignUsersDivisions() throws Exception {
+        divisionAssinger.assignUsersDivisions();
+
+        assertEquals(1, divisionService.findByLeague(League.CHALLENGER).getUsers().size());
+        assertEquals(1, divisionService.findByLeague(League.DIAMOND).getUsers().size());
+        assertEquals(1, divisionService.findByLeague(League.BRONZE).getUsers().size());
+    }
+
+    private User setUpUser1() throws Exception {
+        User user = new User("boss", "boss", "boss");
+
+        Equipment equipment = new Equipment();
+        equipment.setWeapon(new Item(WeaponType.WEAPON, new Statistic(2, 5, 7, 9, 10, 12), 1));
+        equipment.setArmor(new Item(WeaponType.ARMOR, new Statistic(2, 5, 7, 9, 10, 12), 1));
+        equipment.setRing(new Item(WeaponType.RING, new Statistic(2, 5, 7, 9, 10, 12), 1));
+        equipment.setOffhand(new Item(WeaponType.OFFHAND, new Statistic(2, 5, 7, 9, 10, 12), 1));
+        equipment.setNecklace(new Item(WeaponType.NECKLACE, new Statistic(2, 5, 7, 9, 10, 12), 1));
+        equipment.setLegs(new Item(WeaponType.LEGS, new Statistic(2, 5, 7, 9, 10, 12), 1));
+        equipment.setHelmet(new Item(WeaponType.HELMET, new Statistic(2, 5, 7, 9, 10, 12), 1));
+        equipment.setGloves(new Item(WeaponType.GLOVES, new Statistic(2, 5, 7, 9, 10, 12), 1));
+        equipment.setBracelet(new Item(WeaponType.BRACELET, new Statistic(2, 5, 7, 9, 10, 12), 1));
+        equipment.setBoots(new Item(WeaponType.BOOTS, new Statistic(2, 5, 7, 9, 10, 12), 1));
+
+        user.getChampions().add(new Champion(new Statistic(25, 22, 50, 55, 51, 57), equipment).setLevel(10));
+        user.getChampions().add(new Champion(new Statistic(25, 22, 50, 55, 51, 57), equipment).setLevel(10));
+        user.getChampions().add(new Champion(new Statistic(25, 22, 50, 55, 51, 57), equipment).setLevel(100));
+
+        return user;
+    }
+
+    private User setUpUser2() throws Exception {
+        User user = new User("example", "example", "example");
+
+        Equipment equipment = new Equipment();
+        equipment.setWeapon(new Item(WeaponType.WEAPON, new Statistic(0, 0, 0, 0, 0, 0), 1));
+        equipment.setArmor(new Item(WeaponType.ARMOR, new Statistic(0, 0, 0, 0, 0, 0), 1));
+        equipment.setRing(new Item(WeaponType.RING, new Statistic(0, 0, 0, 0, 0, 0), 1));
+        equipment.setOffhand(new Item(WeaponType.OFFHAND, new Statistic(0, 0, 0, 0, 0, 0), 1));
+        equipment.setNecklace(new Item(WeaponType.NECKLACE, new Statistic(0, 0, 0, 0, 0, 0), 1));
+        equipment.setLegs(new Item(WeaponType.LEGS, new Statistic(0, 0, 0, 0, 0, 0), 1));
+        equipment.setHelmet(new Item(WeaponType.HELMET, new Statistic(0, 0, 0, 0, 0, 0), 1));
+        equipment.setGloves(new Item(WeaponType.GLOVES, new Statistic(0, 0, 0, 0, 0, 0), 1));
+        equipment.setBracelet(new Item(WeaponType.BRACELET, new Statistic(0, 0, 0, 0, 0, 0), 1));
+        equipment.setBoots(new Item(WeaponType.BOOTS, new Statistic(0, 0, 0, 0, 0, 0), 1));
+
+        user.getChampions().add(new Champion(new Statistic(1, 1, 60, 0, 1, 1), equipment).setLevel(30));
+
+        return user;
+    }
+
+    private User setUpUser3() throws Exception {
+        User user = new User("medium", "medium", "medium");
+
+        Equipment equipment = new Equipment();
+        equipment.setWeapon(new Item(WeaponType.WEAPON, new Statistic(0, 0, 0, 0, 0, 0), 1));
+        equipment.setArmor(new Item(WeaponType.ARMOR, new Statistic(0, 0, 0, 0, 0, 0), 1));
+        equipment.setRing(new Item(WeaponType.RING, new Statistic(0, 0, 0, 0, 0, 0), 1));
+        equipment.setOffhand(new Item(WeaponType.OFFHAND, new Statistic(0, 0, 0, 0, 0, 0), 1));
+        equipment.setNecklace(new Item(WeaponType.NECKLACE, new Statistic(0, 0, 0, 0, 0, 0), 1));
+        equipment.setLegs(new Item(WeaponType.LEGS, new Statistic(0, 0, 0, 0, 0, 0), 1));
+        equipment.setHelmet(new Item(WeaponType.HELMET, new Statistic(0, 0, 0, 0, 0, 0), 1));
+        equipment.setGloves(new Item(WeaponType.GLOVES, new Statistic(0, 0, 0, 0, 0, 0), 1));
+        equipment.setBracelet(new Item(WeaponType.BRACELET, new Statistic(0, 0, 0, 0, 0, 0), 1));
+        equipment.setBoots(new Item(WeaponType.BOOTS, new Statistic(0, 0, 0, 0, 0, 0), 1));
+
+        user.getChampions().add(new Champion(new Statistic(11, 11, 50, 10, 11, 11), equipment).setLevel(60));
+
+        return user;
+    }
+}
