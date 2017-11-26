d32528fd53cce56f37f41a84aa70ea23b75089c4
xSzymo <szymekssss@wp.pl>
2017-11-26 20:17:00 +0100

    T: 0 - CREATE 100 LEVELS in enum & in systemVariablesManager

--- src/main/java/game/mightywarriors/configuration/system/SystemVariablesManager.java
+++ src/main/java/game/mightywarriors/configuration/system/SystemVariablesManager.java
@@ -7,7 +7,11 @@ import game.mightywarriors.other.Base64.EncoderJSON;
 
 import java.util.LinkedList;
 
-public class SystemVariablesManager {
+public final class SystemVariablesManager {
+    SystemVariablesManager() {
+    }
+
+    ;
 
     public static final String SPECIAL_JWT_SECRET_KEY = "K00LINN3R";
     public static final String NAME_OF_JWT_HEADER_TOKEN = "authorization";
@@ -51,4 +55,117 @@ public class SystemVariablesManager {
     public static final DecoderJSON DECODER_JSON = new DecoderJSON();
 
     public static boolean RUNNING_TESTS = false;
+
+    public final class Levels {
+        public static final long ONE = 10;
+        public static final long TWO = 20;
+        public static final long THREE = 30;
+        public static final long FOUR = 40;
+        public static final long FIVE = 50;
+        public static final long SIX = 60;
+        public static final long SEVEN = 70;
+        public static final long EIGHT = 80;
+        public static final long NINE = 90;
+        public static final long TEN = 100;
+
+        public static final long ELEVEN = 100;
+        public static final long TWELVE = 100;
+        public static final long THIRTEEN = 100;
+        public static final long FOURTEEN = 100;
+        public static final long FIFTEEN = 100;
+        public static final long SIXTEEN = 100;
+        public static final long SEVENTEEN = 100;
+        public static final long EIGHTEEN = 100;
+        public static final long NINETEEN = 100;
+
+        public static final long TWENTY = 100;
+        public static final long TWENTY_ONE = 100;
+        public static final long TWENTY_TWO = 100;
+        public static final long TWENTY_THREE = 100;
+        public static final long TWENTY_FOUR = 100;
+        public static final long TWENTY_FIVE = 100;
+        public static final long TWENTY_SIX = 100;
+        public static final long TWENTY_SEVEN = 100;
+        public static final long TWENTY_EIGHT = 100;
+        public static final long TWENTY_NINE = 100;
+
+        public static final long THIRTY = 100;
+        public static final long THIRTY_ONE = 100;
+        public static final long THIRTY_TWO = 100;
+        public static final long THIRTY_THREE = 100;
+        public static final long THIRTY_FOUR = 100;
+        public static final long THIRTY_FIVE = 100;
+        public static final long THIRTY_SIX = 100;
+        public static final long THIRTY_SEVEN = 100;
+        public static final long THIRTY_EIGHT = 100;
+        public static final long THIRTY_NINE = 100;
+
+        public static final long FORTY = 100;
+        public static final long FORTY_ONE = 100;
+        public static final long FORTY_TWO = 100;
+        public static final long FORTY_THREE = 100;
+        public static final long FORTY_FOUR = 100;
+        public static final long FORTY_FIVE = 100;
+        public static final long FORTY_SIX = 100;
+        public static final long FORTY_SEVEN = 100;
+        public static final long FORTY_EIGHT = 100;
+        public static final long FORTY_NINE = 100;
+
+        public static final long FIFTY = 100;
+        public static final long FIFTY_ONE = 100;
+        public static final long FIFTY_TWO = 100;
+        public static final long FIFTY_THREE = 100;
+        public static final long FIFTY_FOUR = 100;
+        public static final long FIFTY_FIVE = 100;
+        public static final long FIFTY_SIX = 100;
+        public static final long FIFTY_SEVEN = 100;
+        public static final long FIFTY_EIGHT = 100;
+        public static final long FIFTY_NINE = 100;
+
+        public static final long SIXTY = 100;
+        public static final long SIXTY_ONE = 100;
+        public static final long SIXTY_TWO = 100;
+        public static final long SIXTY_THREE = 100;
+        public static final long SIXTY_FOUR = 100;
+        public static final long SIXTY_FIVE = 100;
+        public static final long SIXTY_SIX = 100;
+        public static final long SIXTY_SEVEN = 100;
+        public static final long SIXTY_EIGHT = 100;
+        public static final long SIXTY_NINE = 100;
+
+        public static final long SEVENTY = 100;
+        public static final long SEVENTY_ONE = 100;
+        public static final long SEVENTY_TWO = 100;
+        public static final long SEVENTY_THREE = 100;
+        public static final long SEVENTY_FOUR = 100;
+        public static final long SEVENTY_FIVE = 100;
+        public static final long SEVENTY_SIX = 100;
+        public static final long SEVENTY_SEVEN = 100;
+        public static final long SEVENTY_EIGHT = 100;
+        public static final long SEVENTY_NINE = 100;
+
+        public static final long EIGHTY = 100;
+        public static final long EIGHTY_ONE = 100;
+        public static final long EIGHTY_TWO = 100;
+        public static final long EIGHTY_THREE = 100;
+        public static final long EIGHTY_FOUR = 100;
+        public static final long EIGHTY_FIVE = 100;
+        public static final long EIGHTY_SIX = 100;
+        public static final long EIGHTY_SEVEN = 100;
+        public static final long EIGHTY_EIGHT = 100;
+        public static final long EIGHTY_NINE = 100;
+
+        public static final long NINETY = 100;
+        public static final long NINETY_ONE = 100;
+        public static final long NINETY_TWO = 100;
+        public static final long NINETY_THREE = 100;
+        public static final long NINETY_FOUR = 100;
+        public static final long NINETY_FIVE = 100;
+        public static final long NINETY_SIX = 100;
+        public static final long NINETY_SEVEN = 100;
+        public static final long NINETY_EIGHT = 100;
+        public static final long NINETY_NINE = 100;
+
+        public static final long ONE_HUNDRED = 1000;
+    }
 }
--- src/main/java/game/mightywarriors/other/enums/Level.java
+++ src/main/java/game/mightywarriors/other/enums/Level.java
@@ -1,8 +1,58 @@
 package game.mightywarriors.other.enums;
 
+import game.mightywarriors.configuration.system.SystemVariablesManager;
+
 public enum Level {
-    ONE(10, 1), TWO(20, 2), THREE(40, 3), FOUR(80, 4), FIVE(160, 5), SIX(320, 6), SEVEN(640, 7), EIGHT(1280, 8), NINE(2560, 9), TEN(5120, 10),
-    ELEVEN(10240, 11), TWELVE(20480, 12);
+    ONE(SystemVariablesManager.Levels.ONE, 1), TWO(SystemVariablesManager.Levels.TWO, 2), THREE(SystemVariablesManager.Levels.THREE, 3),
+    FOUR(SystemVariablesManager.Levels.FOUR, 4), FIVE(SystemVariablesManager.Levels.FIVE, 5), SIX(SystemVariablesManager.Levels.SIX, 6),
+    SEVEN(SystemVariablesManager.Levels.SEVEN, 7), EIGHT(SystemVariablesManager.Levels.EIGHT, 8), NINE(SystemVariablesManager.Levels.NINE, 9),
+
+    TEN(SystemVariablesManager.Levels.TEN, 10),
+    ELEVEN(SystemVariablesManager.Levels.ELEVEN, 11), TWELVE(SystemVariablesManager.Levels.TWELVE, 12), THIRTEEN(SystemVariablesManager.Levels.THIRTEEN, 13),
+    FOURTEEN(SystemVariablesManager.Levels.FOURTEEN, 14), FIFTEEN(SystemVariablesManager.Levels.FIFTEEN, 15), SIXTEEN(SystemVariablesManager.Levels.SIXTEEN, 16),
+    SEVENTEEN(SystemVariablesManager.Levels.SEVENTEEN, 17), EIGHTEEN(SystemVariablesManager.Levels.EIGHTEEN, 18), NINETEEN(SystemVariablesManager.Levels.NINETEEN, 29),
+
+    TWENTY(SystemVariablesManager.Levels.TWENTY, 20),
+    TWENTY_ONE(SystemVariablesManager.Levels.TWENTY_ONE, 21), TWENTY_TWO(SystemVariablesManager.Levels.TWENTY_TWO, 22), TWENTY_THREE(SystemVariablesManager.Levels.TWENTY_THREE, 23),
+    TWENTY_FOUR(SystemVariablesManager.Levels.TWENTY_FOUR, 24), TWENTY_FIVE(SystemVariablesManager.Levels.TWENTY_FIVE, 25), TWENTY_SIX(SystemVariablesManager.Levels.TWENTY_SIX, 26),
+    TWENTY_SEVEN(SystemVariablesManager.Levels.TWENTY_SEVEN, 27), TWENTY_EIGHT(SystemVariablesManager.Levels.TWENTY_EIGHT, 28), TWENTY_NINE(SystemVariablesManager.Levels.TWENTY_NINE, 29),
+
+    THIRTY(SystemVariablesManager.Levels.THIRTY, 30),
+    THIRTY_ONE(SystemVariablesManager.Levels.THIRTY_ONE, 31), THIRTY_TWO(SystemVariablesManager.Levels.THIRTY_TWO, 32), THIRTY_THREE(SystemVariablesManager.Levels.THIRTY_THREE, 33),
+    THIRTY_FOUR(SystemVariablesManager.Levels.THIRTY_FOUR, 34), THIRTY_FIVE(SystemVariablesManager.Levels.THIRTY_FIVE, 35), THIRTY_SIX(SystemVariablesManager.Levels.THIRTY_SIX, 36),
+    THIRTY_SEVEN(SystemVariablesManager.Levels.THIRTY_SEVEN, 37), THIRTY_EIGHT(SystemVariablesManager.Levels.THIRTY_EIGHT, 38), THIRTY_NINE(SystemVariablesManager.Levels.THIRTY_NINE, 39),
+
+    FORTY(SystemVariablesManager.Levels.FORTY, 40),
+    FORTY_ONE(SystemVariablesManager.Levels.FORTY_ONE, 41), FORTY_TWO(SystemVariablesManager.Levels.FORTY_TWO, 42), FORTY_THREE(SystemVariablesManager.Levels.FORTY_THREE, 43),
+    FORTY_FOUR(SystemVariablesManager.Levels.FORTY_FOUR, 44), FORTY_FIVE(SystemVariablesManager.Levels.FORTY_FIVE, 45), FORTY_SIX(SystemVariablesManager.Levels.FORTY_SIX, 46),
+    FORTY_SEVEN(SystemVariablesManager.Levels.FORTY_SEVEN, 47), FORTY_EIGHT(SystemVariablesManager.Levels.FORTY_EIGHT, 48), FORTY_NINE(SystemVariablesManager.Levels.FORTY_NINE, 49),
+
+    FIFTY(SystemVariablesManager.Levels.FIFTY, 50),
+    FIFTY_ONE(SystemVariablesManager.Levels.FIFTY_ONE, 51), FIFTY_TWO(SystemVariablesManager.Levels.FIFTY_TWO, 52), FIFTY_THREE(SystemVariablesManager.Levels.FIFTY_THREE, 53),
+    FIFTY_FOUR(SystemVariablesManager.Levels.FIFTY_FOUR, 54), FIFTY_FIVE(SystemVariablesManager.Levels.FIFTY_FIVE, 55), FIFTY_SIX(SystemVariablesManager.Levels.FIFTY_SIX, 56),
+    FIFTY_SEVEN(SystemVariablesManager.Levels.FIFTY_SEVEN, 57), FIFTY_EIGHT(SystemVariablesManager.Levels.FIFTY_EIGHT, 58), FIFTY_NINE(SystemVariablesManager.Levels.FIFTY_NINE, 59),
+
+    SIXTY(SystemVariablesManager.Levels.SIXTY, 60),
+    SIXTY_ONE(SystemVariablesManager.Levels.SIXTY_ONE, 61), SIXTY_TWO(SystemVariablesManager.Levels.SIXTY_TWO, 62), SIXTY_THREE(SystemVariablesManager.Levels.SIXTY_THREE, 63),
+    SIXTY_FOUR(SystemVariablesManager.Levels.SIXTY_FOUR, 64), SIXTY_FIVE(SystemVariablesManager.Levels.SIXTY_FIVE, 65), SIXTY_SIX(SystemVariablesManager.Levels.SIXTY_SIX, 66),
+    SIXTY_SEVEN(SystemVariablesManager.Levels.SIXTY_SEVEN, 67), SIXTY_EIGHT(SystemVariablesManager.Levels.SIXTY_EIGHT, 68), SIXTY_NINE(SystemVariablesManager.Levels.SIXTY_NINE, 69),
+
+    SEVENTY(SystemVariablesManager.Levels.SEVENTY, 70),
+    SEVENTY_ONE(SystemVariablesManager.Levels.SEVENTY_ONE, 71), SEVENTY_TWO(SystemVariablesManager.Levels.SEVENTY_TWO, 72), SEVENTY_THREE(SystemVariablesManager.Levels.SEVENTY_THREE, 73),
+    SEVENTY_FOUR(SystemVariablesManager.Levels.SEVENTY_FOUR, 74), SEVENTY_FIVE(SystemVariablesManager.Levels.SEVENTY_FIVE, 75), SEVENTY_SIX(SystemVariablesManager.Levels.SEVENTY_SIX, 76),
+    SEVENTY_SEVEN(SystemVariablesManager.Levels.SEVENTY_SEVEN, 77), SEVENTY_EIGHT(SystemVariablesManager.Levels.SEVENTY_EIGHT, 78), SEVENTY_NINE(SystemVariablesManager.Levels.SEVENTY_NINE, 79),
+
+    EIGHTY(SystemVariablesManager.Levels.EIGHTY, 80),
+    EIGHTY_ONE(SystemVariablesManager.Levels.EIGHTY_ONE, 81), EIGHTY_TWO(SystemVariablesManager.Levels.EIGHTY_TWO, 82), EIGHTY_THREE(SystemVariablesManager.Levels.EIGHTY_THREE, 83),
+    EIGHTY_FOUR(SystemVariablesManager.Levels.EIGHTY_FOUR, 84), EIGHTY_FIVE(SystemVariablesManager.Levels.EIGHTY_FIVE, 85), EIGHTY_SIX(SystemVariablesManager.Levels.EIGHTY_SIX, 86),
+    EIGHTY_SEVEN(SystemVariablesManager.Levels.EIGHTY_SEVEN, 87), EIGHTY_EIGHT(SystemVariablesManager.Levels.EIGHTY_EIGHT, 88), EIGHTY_NINE(SystemVariablesManager.Levels.EIGHTY_NINE, 89),
+
+    NINETY(SystemVariablesManager.Levels.NINETY, 90),
+    NINETY_ONE(SystemVariablesManager.Levels.NINETY_ONE, 91), NINETY_TWO(SystemVariablesManager.Levels.NINETY_TWO, 92), NINETY_THREE(SystemVariablesManager.Levels.NINETY_THREE, 93),
+    NINETY_FOUR(SystemVariablesManager.Levels.NINETY_FOUR, 94), NINETY_FIVE(SystemVariablesManager.Levels.NINETY_FIVE, 95), NINETY_SIX(SystemVariablesManager.Levels.NINETY_SIX, 96),
+    NINETY_SEVEN(SystemVariablesManager.Levels.NINETY_SEVEN, 97), NINETY_EIGHT(SystemVariablesManager.Levels.NINETY_EIGHT, 98), NINETY_NINE(SystemVariablesManager.Levels.NINETY_NINE, 99),
+
+    ONE_HUNDRED(SystemVariablesManager.Levels.ONE_HUNDRED, 100);
 
     private long experience;
     private long level;
