f81fbff8c632694d4414800854354492bfe05895
xSzymo <szymekssss@wp.pl>
2017-11-26 18:43:53 +0100

    T: 10 - add static variables for division system

--- src/main/java/game/mightywarriors/configuration/system/SystemVariablesManager.java
+++ src/main/java/game/mightywarriors/configuration/system/SystemVariablesManager.java
@@ -12,16 +12,38 @@ public class SystemVariablesManager {
     public static final String SPECIAL_JWT_SECRET_KEY = "K00LINN3R";
     public static final String NAME_OF_JWT_HEADER_TOKEN = "authorization";
     public static final int HOW_MANY_HOURS_BETWEEN_NEXT_DRAW_ITEMS = 1;
+    public static final int HOW_MANY_HOURS_BETWEEN_UPDAATE_DIVISIONS = 1;
     public static final double RATE = 0.1;
     public static final int HP_RATE = 3;
 
     public static LinkedList<String> JWTTokenCollection = new LinkedList<>();
 
+    public static final long MAX_PLAYERS_IN_CHALLENGER = 2;
+    public static final long MAX_PLAYERS_IN_DIAMOND = 5;
+    public static final long MAX_PERCENT_PLAYERS_IN_GOLD = 10;
+    public static final long MAX_PERCENT_PLAYERS_IN_SILVER = 20;
+    public static final long MAX_PERCENT_PLAYERS_IN_BRONZE = 30;
+    public static final long MAX_PERCENT_PLAYERS_IN_WOOD = 40;
+
+    public static final long MIN_LEVEL_FOR_CHALLENGER = 100;
+    public static final long MIN_LEVEL_FOR_DIAMOND = 60;
+    public static final long MIN_LEVEL_FOR_GOLD = 50;
+    public static final long MIN_LEVEL_FOR_SILVER = 40;
+    public static final long MIN_LEVEL_FOR_BRONZE = 30;
+    public static final long MIN_LEVEL_FOR_WOOD = 10;
+
+    public static final long MIN_POINTS_FOR_CHALLENGER = 800;
+    public static final long MIN_POINTS_FOR_DIAMOND = 500;
+    public static final long MIN_POINTS_FOR_GOLD = 300;
+    public static final long MIN_POINTS_FOR_SILVER = 100;
+    public static final long MIN_POINTS_FOR_BRONZE = 50;
+    public static final long MIN_POINTS_FOR_WOOD = 10;
+
     public static final int NUMBER_OF_FIRST_CHAR_TO_DELETE = 6;
     public static final int NUMBER_OF_SECOND_CHAR_TO_DELETE = 14;
     public static final int NUMBER_OF_THIRD_CHAR_TO_DELETE = 16;
     public static final int NUMBER_OF_FOURTH_CHAR_TO_DELETE = 26;
-    public static final int SECONDS_FOR_TOKEN_EXPIRED = 48*60*60;
+    public static final int SECONDS_FOR_TOKEN_EXPIRED = 48 * 60 * 60;
 
     public static final DecoderDB DECO4DER_DB = new DecoderDB();
     public static final EncoderDB ENCODER_DB = new EncoderDB();
