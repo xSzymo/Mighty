705db58fb999a27fe47cbd6ae803313009653319
xSzymo <szymekssss@wp.pl>
2017-12-14 22:28:18 +0100

    T: 17 - deleted JWTToken after ENCODERJSONTest

--- src/test/java/unit/other/Base64/EncoderJSONTest.java
+++ src/test/java/unit/other/Base64/EncoderJSONTest.java
@@ -2,16 +2,23 @@ package unit.other.Base64;
 
 import game.mightywarriors.configuration.system.SystemVariablesManager;
 import game.mightywarriors.other.generators.RandomCodeFactory;
+import org.junit.After;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class EncoderJSONTest {
+    private String uniqueCode;
+
+    @After
+    public void cleanUp() {
+        SystemVariablesManager.JWTTokenCollection.remove(uniqueCode);
+    }
 
     @Test
     public void encode() {
         RandomCodeFactory randomCodeFactory = new RandomCodeFactory();
-        String uniqueCode = randomCodeFactory.getUniqueCode();
+        uniqueCode = randomCodeFactory.getUniqueCode();
 
         String jsonCode = SystemVariablesManager.ENCODER_JSON.encode(uniqueCode);
         String dbCode = SystemVariablesManager.ENCODER_DB.encode(uniqueCode);
