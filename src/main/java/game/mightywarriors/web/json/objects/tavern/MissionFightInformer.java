6073dd379e4a51422c9b92cdda17e4ef4c2750ef
xSzymo <szymekssss@wp.pl>
2017-12-17 23:42:27 +0100

    T: 23 - add missionFightId & change championId to table

--- src/main/java/game/mightywarriors/web/json/objects/tavern/MissionFightInformer.java
+++ src/main/java/game/mightywarriors/web/json/objects/tavern/MissionFightInformer.java
@@ -1,6 +1,7 @@
 package game.mightywarriors.web.json.objects.tavern;
 
 public class MissionFightInformer {
+    public long missionFightId;
     public long missionId;
-    public long championId;
+    public long[] championId;
 }
