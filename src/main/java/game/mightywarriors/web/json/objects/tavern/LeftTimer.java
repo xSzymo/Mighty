54b30de8a390e6b568de8e86cc94b2882d74d595
xSzymo <szymekssss@wp.pl>
2017-12-17 02:07:10 +0100

    T: 23 - add 2 json representation classes for tavern

---
+++ src/main/java/game/mightywarriors/web/json/objects/tavern/LeftTimer.java
@@ -0,0 +1,9 @@
+package game.mightywarriors.web.json.objects.tavern;
+
+public class LeftTimer {
+    public long leftTime;
+
+    public LeftTimer(long leftTime) {
+        this.leftTime = leftTime;
+    }
+}
---
+++ src/main/java/game/mightywarriors/web/json/objects/tavern/MissionFightInformator.java
@@ -0,0 +1,6 @@
+package game.mightywarriors.web.json.objects.tavern;
+
+public class MissionFightInformator {
+    public long missionId;
+    public long championId;
+}
