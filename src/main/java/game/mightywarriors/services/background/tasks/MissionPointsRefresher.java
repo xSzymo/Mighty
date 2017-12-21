c17f0a45f0436a41a02b7e5f52cd31e23f65166a
xSzymo <szymekssss@wp.pl>
2017-12-21 22:50:46 +0100

    T: 23 - create MissionPointsRefresher & test

---
+++ src/main/java/game/mightywarriors/services/background/tasks/MissionPointsRefresher.java
@@ -0,0 +1,48 @@
+package game.mightywarriors.services.background.tasks;
+
+import game.mightywarriors.configuration.system.SystemVariablesManager;
+import game.mightywarriors.data.services.UserService;
+import game.mightywarriors.data.tables.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.LinkedList;
+
+@Service
+public class MissionPointsRefresher {
+    @Autowired
+    private UserService userService;
+
+    @Transactional
+    public void refreshMissionPointsForAllMission() {
+        LinkedList<User> users = userService.findAll();
+
+        users.forEach(x -> {
+            long level = x.getUserChampiongHighestLevel();
+
+            if (level >= 0 && level <= 10)
+                x.setMissionPoints(SystemVariablesManager.HOW_MANY_HOURS_MISSIONS_BETWEEN_LEVEL_1_AND_10);
+            else if (level >= 11 && level <= 20)
+                x.setMissionPoints(SystemVariablesManager.HOW_MANY_HOURS_MISSIONS_BETWEEN_LEVEL_11_AND_20);
+            else if (level >= 21 && level <= 30)
+                x.setMissionPoints(SystemVariablesManager.HOW_MANY_HOURS_MISSIONS_BETWEEN_LEVEL_21_AND_30);
+            else if (level >= 31 && level <= 40)
+                x.setMissionPoints(SystemVariablesManager.HOW_MANY_HOURS_MISSIONS_BETWEEN_LEVEL_31_AND_40);
+            else if (level >= 41 && level <= 50)
+                x.setMissionPoints(SystemVariablesManager.HOW_MANY_HOURS_MISSIONS_BETWEEN_LEVEL_41_AND_50);
+            else if (level >= 51 && level <= 60)
+                x.setMissionPoints(SystemVariablesManager.HOW_MANY_HOURS_MISSIONS_BETWEEN_LEVEL_51_AND_60);
+            else if (level >= 61 && level <= 70)
+                x.setMissionPoints(SystemVariablesManager.HOW_MANY_HOURS_MISSIONS_BETWEEN_LEVEL_61_AND_70);
+            else if (level >= 71 && level <= 80)
+                x.setMissionPoints(SystemVariablesManager.HOW_MANY_HOURS_MISSIONS_BETWEEN_LEVEL_71_AND_80);
+            else if (level >= 81 && level <= 90)
+                x.setMissionPoints(SystemVariablesManager.HOW_MANY_HOURS_MISSIONS_BETWEEN_LEVEL_81_AND_90);
+            else if (level >= 91 && level <= 100)
+                x.setMissionPoints(SystemVariablesManager.HOW_MANY_HOURS_MISSIONS_BETWEEN_LEVEL_91_AND_100);
+        });
+
+        userService.save(users);
+    }
+}
---
+++ src/test/java/integration/services/background/tasks/MissionPointsRefresherTest.java
@@ -0,0 +1,52 @@
+package integration.services.background.tasks;
+
+import game.mightywarriors.configuration.system.SystemVariablesManager;
+import game.mightywarriors.data.services.UserService;
+import game.mightywarriors.data.tables.User;
+import game.mightywarriors.services.background.tasks.MissionPointsRefresher;
+import integration.config.IntegrationTestsConfig;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.LinkedList;
+
+import static org.junit.Assert.assertEquals;
+
+public class MissionPointsRefresherTest extends IntegrationTestsConfig {
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private MissionPointsRefresher missionPointsRefresher;
+
+    @Test
+    public void refreshMissionPointsForAllMission() {
+        missionPointsRefresher.refreshMissionPointsForAllMission();
+
+        LinkedList<User> users = userService.findAll();
+
+        users.forEach(x -> {
+            long level = x.getUserChampiongHighestLevel();
+
+            if (level >= 0 && level <= 10)
+                assertEquals(SystemVariablesManager.HOW_MANY_HOURS_MISSIONS_BETWEEN_LEVEL_1_AND_10, level);
+            else if (level >= 11 && level <= 20)
+                assertEquals(SystemVariablesManager.HOW_MANY_HOURS_MISSIONS_BETWEEN_LEVEL_11_AND_20, level);
+            else if (level >= 21 && level <= 30)
+                assertEquals(SystemVariablesManager.HOW_MANY_HOURS_MISSIONS_BETWEEN_LEVEL_21_AND_30, level);
+            else if (level >= 31 && level <= 40)
+                assertEquals(SystemVariablesManager.HOW_MANY_HOURS_MISSIONS_BETWEEN_LEVEL_31_AND_40, level);
+            else if (level >= 41 && level <= 50)
+                assertEquals(SystemVariablesManager.HOW_MANY_HOURS_MISSIONS_BETWEEN_LEVEL_41_AND_50, level);
+            else if (level >= 51 && level <= 60)
+                assertEquals(SystemVariablesManager.HOW_MANY_HOURS_MISSIONS_BETWEEN_LEVEL_51_AND_60, level);
+            else if (level >= 61 && level <= 70)
+                assertEquals(SystemVariablesManager.HOW_MANY_HOURS_MISSIONS_BETWEEN_LEVEL_61_AND_70, level);
+            else if (level >= 71 && level <= 80)
+                assertEquals(SystemVariablesManager.HOW_MANY_HOURS_MISSIONS_BETWEEN_LEVEL_71_AND_80, level);
+            else if (level >= 81 && level <= 90)
+                assertEquals(SystemVariablesManager.HOW_MANY_HOURS_MISSIONS_BETWEEN_LEVEL_81_AND_90, level);
+            else if (level >= 91 && level <= 100)
+                assertEquals(SystemVariablesManager.HOW_MANY_HOURS_MISSIONS_BETWEEN_LEVEL_91_AND_100, level);
+        });
+    }
+}
