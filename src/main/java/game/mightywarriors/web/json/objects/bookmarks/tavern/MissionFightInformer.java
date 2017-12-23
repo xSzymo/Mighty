9bedf17eb6356589bd240d994294680247590066
xSzymo <szymekssss@wp.pl>
2017-12-23 01:29:09 +0100

    T: 27 - add a few variables json representation for tavern

--- src/main/java/game/mightywarriors/web/json/objects/bookmarks/tavern/MissionFightInformer.java
+++ src/main/java/game/mightywarriors/web/json/objects/bookmarks/tavern/MissionFightInformer.java
@@ -1,7 +1,29 @@
 package game.mightywarriors.web.json.objects.bookmarks.tavern;
 
 public class MissionFightInformer {
+
+    /**
+     * Mission
+     * id : MissionFight id
+     * missionId : mission id
+     */
     public long id;
     public long missionId;
+
+    /**
+     * Tavern
+     * Can use one of them to find opponent
+     *
+     * opponentId : opponent's id to fight
+     * opponentName :  opponent's name to fight
+     */
+    public long opponentId;
+    public String opponentName;
+
+    /**
+     * Both
+     * championId : table of user's champions ids
+     */
     public long[] championId;
+
 }
