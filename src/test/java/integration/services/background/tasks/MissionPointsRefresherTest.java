515a5b17172eddc26c04bea9a18d9aa59088c1ff
xSzymo <szymekssss@wp.pl>
2017-12-24 20:53:43 +0100

    T: 0 - fix MissionPointsRefresherTest NO COMMENT XD

--- src/test/java/integration/services/background/tasks/MissionPointsRefresherTest.java
+++ src/test/java/integration/services/background/tasks/MissionPointsRefresherTest.java
@@ -5,6 +5,7 @@ import game.mightywarriors.data.services.UserService;
 import game.mightywarriors.data.tables.User;
 import game.mightywarriors.services.background.tasks.MissionPointsRefresher;
 import integration.config.IntegrationTestsConfig;
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -18,35 +19,44 @@ public class MissionPointsRefresherTest extends IntegrationTestsConfig {
     @Autowired
     private MissionPointsRefresher missionPointsRefresher;
 
+    @Before
+    public void setUp() {
+        for (int i = 0; i < 10; i++) {
+            userService.save(new User("" + i, "", ""));
+        }
+    }
+
     @Test
     public void refreshMissionPointsForAllMission() {
+        LinkedList<User> users = userService.findAll();
+        users.forEach(x -> x.setMissionPoints(213));
+        userService.save(users);
+
         missionPointsRefresher.refreshMissionPointsForAllMission();
 
-        LinkedList<User> users = userService.findAll();
-
-        users.forEach(x -> {
+        userService.findAll().forEach(x -> {
             long level = x.getUserChampiongHighestLevel();
 
             if (level >= 0 && level <= 10)
-                assertEquals(SystemVariablesManager.POINTS_MISSIONS_BETWEEN_LEVEL_1_AND_10, level);
+                assertEquals(SystemVariablesManager.POINTS_MISSIONS_BETWEEN_LEVEL_1_AND_10, x.getMissionPoints());
             else if (level >= 11 && level <= 20)
-                assertEquals(SystemVariablesManager.POINTS_MISSIONS_BETWEEN_LEVEL_11_AND_20, level);
+                assertEquals(SystemVariablesManager.POINTS_MISSIONS_BETWEEN_LEVEL_11_AND_20, x.getMissionPoints());
             else if (level >= 21 && level <= 30)
-                assertEquals(SystemVariablesManager.POINTS_MISSIONS_BETWEEN_LEVEL_21_AND_30, level);
+                assertEquals(SystemVariablesManager.POINTS_MISSIONS_BETWEEN_LEVEL_21_AND_30, x.getMissionPoints());
             else if (level >= 31 && level <= 40)
-                assertEquals(SystemVariablesManager.POINTS_MISSIONS_BETWEEN_LEVEL_31_AND_40, level);
+                assertEquals(SystemVariablesManager.POINTS_MISSIONS_BETWEEN_LEVEL_31_AND_40, x.getMissionPoints());
             else if (level >= 41 && level <= 50)
-                assertEquals(SystemVariablesManager.POINTS_MISSIONS_BETWEEN_LEVEL_41_AND_50, level);
+                assertEquals(SystemVariablesManager.POINTS_MISSIONS_BETWEEN_LEVEL_41_AND_50, x.getMissionPoints());
             else if (level >= 51 && level <= 60)
-                assertEquals(SystemVariablesManager.POINTS_MISSIONS_BETWEEN_LEVEL_51_AND_60, level);
+                assertEquals(SystemVariablesManager.POINTS_MISSIONS_BETWEEN_LEVEL_51_AND_60, x.getMissionPoints());
             else if (level >= 61 && level <= 70)
-                assertEquals(SystemVariablesManager.POINTS_MISSIONS_BETWEEN_LEVEL_61_AND_70, level);
+                assertEquals(SystemVariablesManager.POINTS_MISSIONS_BETWEEN_LEVEL_61_AND_70, x.getMissionPoints());
             else if (level >= 71 && level <= 80)
-                assertEquals(SystemVariablesManager.POINTS_MISSIONS_BETWEEN_LEVEL_71_AND_80, level);
+                assertEquals(SystemVariablesManager.POINTS_MISSIONS_BETWEEN_LEVEL_71_AND_80, x.getMissionPoints());
             else if (level >= 81 && level <= 90)
-                assertEquals(SystemVariablesManager.POINTS_MISSIONS_BETWEEN_LEVEL_81_AND_90, level);
+                assertEquals(SystemVariablesManager.POINTS_MISSIONS_BETWEEN_LEVEL_81_AND_90, x.getMissionPoints());
             else if (level >= 91 && level <= 100)
-                assertEquals(SystemVariablesManager.POINTS_MISSIONS_BETWEEN_LEVEL_91_AND_100, level);
+                assertEquals(SystemVariablesManager.POINTS_MISSIONS_BETWEEN_LEVEL_91_AND_100, x.getMissionPoints());
         });
     }
 }
