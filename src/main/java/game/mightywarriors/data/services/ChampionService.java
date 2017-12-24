ebfe9ec123098789c8fb3d7527aa645d673c8eef
xSzymo <szymekssss@wp.pl>
2017-12-24 07:20:31 +0100

    T: 30 - create equipment & statistic for new champions

--- src/main/java/game/mightywarriors/data/services/ChampionService.java
+++ src/main/java/game/mightywarriors/data/services/ChampionService.java
@@ -1,7 +1,10 @@
 package game.mightywarriors.data.services;
 
+import game.mightywarriors.configuration.system.SystemVariablesManager;
 import game.mightywarriors.data.repositories.ChampionRepository;
 import game.mightywarriors.data.tables.Champion;
+import game.mightywarriors.data.tables.Equipment;
+import game.mightywarriors.data.tables.Statistic;
 import game.mightywarriors.other.managers.ChampionLevelManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -38,6 +41,8 @@ public class ChampionService {
     }
 
     private void saveOperation(Champion champion) {
+        Champion foundChampion = findOne(champion);
+
         if (champion.getImage() != null)
             try {
                 imageService.save(champion.getImage());
@@ -48,6 +53,22 @@ public class ChampionService {
             statisticService.save(champion.getStatistic());
         if (champion.getEquipment() != null)
             equipmentService.save(champion.getEquipment());
+
+        if (foundChampion == null) {
+            if (champion.getEquipment() == null) {
+                Equipment equipment = new Equipment();
+                equipmentService.save(equipment);
+                champion.setEquipment(equipment);
+            }
+
+            if (champion.getStatistic() == null) {
+                Statistic statistic = new Statistic(SystemVariablesManager.DEFAULT_STRENGTH_FOR_NEW_CHAMPION, SystemVariablesManager.DEFAULT_INTELLIGENCE_FOR_NEW_CHAMPION,
+                        SystemVariablesManager.DEFAULT_VITALITY_FOR_NEW_CHAMPION, SystemVariablesManager.DEFAULT_CRITIC_CHANCE_FOR_NEW_CHAMPION,
+                        SystemVariablesManager.DEFAULT_ARMOR_FOR_NEW_CHAMPION, SystemVariablesManager.DEFAULT_MAGIC_RESIST_FOR_NEW_CHAMPION);
+                statisticService.save(statistic);
+                champion.setStatistic(statistic);
+            }
+        }
 
         champion = ChampionLevelManager.getChampionRealLevel(champion);
         repository.save(champion);
@@ -113,6 +134,8 @@ public class ChampionService {
                 }
         );
 
+        if(champion.getImage() == null)
+            imageService.delete(champion.getImage());
         repository.deleteById(champion.getId());
     }
 }
