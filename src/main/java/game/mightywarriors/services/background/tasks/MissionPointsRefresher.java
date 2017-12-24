3b18cd21be69839370d2a56e469233764eedfda5
xSzymo <szymekssss@wp.pl>
2017-12-24 03:53:33 +0100

    T: 30 - add default variables clean up and back to latest SVM

--- src/main/java/game/mightywarriors/services/background/tasks/MissionPointsRefresher.java
+++ src/main/java/game/mightywarriors/services/background/tasks/MissionPointsRefresher.java
@@ -22,25 +22,25 @@ public class MissionPointsRefresher {
             long level = x.getUserChampiongHighestLevel();
 
             if (level >= 0 && level <= 10)
-                x.setMissionPoints(SystemVariablesManager.HOW_MANY_POINTS_MISSIONS_BETWEEN_LEVEL_1_AND_10);
+                x.setMissionPoints(SystemVariablesManager.POINTS_MISSIONS_BETWEEN_LEVEL_1_AND_10);
             else if (level >= 11 && level <= 20)
-                x.setMissionPoints(SystemVariablesManager.HOW_MANY_POINTS_MISSIONS_BETWEEN_LEVEL_11_AND_20);
+                x.setMissionPoints(SystemVariablesManager.POINTS_MISSIONS_BETWEEN_LEVEL_11_AND_20);
             else if (level >= 21 && level <= 30)
-                x.setMissionPoints(SystemVariablesManager.HOW_MANY_POINTS_MISSIONS_BETWEEN_LEVEL_21_AND_30);
+                x.setMissionPoints(SystemVariablesManager.POINTS_MISSIONS_BETWEEN_LEVEL_21_AND_30);
             else if (level >= 31 && level <= 40)
-                x.setMissionPoints(SystemVariablesManager.HOW_MANY_POINTS_MISSIONS_BETWEEN_LEVEL_31_AND_40);
+                x.setMissionPoints(SystemVariablesManager.POINTS_MISSIONS_BETWEEN_LEVEL_31_AND_40);
             else if (level >= 41 && level <= 50)
-                x.setMissionPoints(SystemVariablesManager.HOW_MANY_POINTS_MISSIONS_BETWEEN_LEVEL_41_AND_50);
+                x.setMissionPoints(SystemVariablesManager.POINTS_MISSIONS_BETWEEN_LEVEL_41_AND_50);
             else if (level >= 51 && level <= 60)
-                x.setMissionPoints(SystemVariablesManager.HOW_MANY_POINTS_MISSIONS_BETWEEN_LEVEL_51_AND_60);
+                x.setMissionPoints(SystemVariablesManager.POINTS_MISSIONS_BETWEEN_LEVEL_51_AND_60);
             else if (level >= 61 && level <= 70)
-                x.setMissionPoints(SystemVariablesManager.HOW_MANY_POINTS_MISSIONS_BETWEEN_LEVEL_61_AND_70);
+                x.setMissionPoints(SystemVariablesManager.POINTS_MISSIONS_BETWEEN_LEVEL_61_AND_70);
             else if (level >= 71 && level <= 80)
-                x.setMissionPoints(SystemVariablesManager.HOW_MANY_POINTS_MISSIONS_BETWEEN_LEVEL_71_AND_80);
+                x.setMissionPoints(SystemVariablesManager.POINTS_MISSIONS_BETWEEN_LEVEL_71_AND_80);
             else if (level >= 81 && level <= 90)
-                x.setMissionPoints(SystemVariablesManager.HOW_MANY_POINTS_MISSIONS_BETWEEN_LEVEL_81_AND_90);
+                x.setMissionPoints(SystemVariablesManager.POINTS_MISSIONS_BETWEEN_LEVEL_81_AND_90);
             else if (level >= 91 && level <= 100)
-                x.setMissionPoints(SystemVariablesManager.HOW_MANY_POINTS_MISSIONS_BETWEEN_LEVEL_91_AND_100);
+                x.setMissionPoints(SystemVariablesManager.POINTS_MISSIONS_BETWEEN_LEVEL_91_AND_100);
         });
 
         userService.save(users);
--- src/test/java/integration/services/background/tasks/MissionPointsRefresherTest.java
+++ src/test/java/integration/services/background/tasks/MissionPointsRefresherTest.java
@@ -28,25 +28,25 @@ public class MissionPointsRefresherTest extends IntegrationTestsConfig {
             long level = x.getUserChampiongHighestLevel();
 
             if (level >= 0 && level <= 10)
-                assertEquals(SystemVariablesManager.HOW_MANY_POINTS_MISSIONS_BETWEEN_LEVEL_1_AND_10, level);
+                assertEquals(SystemVariablesManager.POINTS_MISSIONS_BETWEEN_LEVEL_1_AND_10, level);
             else if (level >= 11 && level <= 20)
-                assertEquals(SystemVariablesManager.HOW_MANY_POINTS_MISSIONS_BETWEEN_LEVEL_11_AND_20, level);
+                assertEquals(SystemVariablesManager.POINTS_MISSIONS_BETWEEN_LEVEL_11_AND_20, level);
             else if (level >= 21 && level <= 30)
-                assertEquals(SystemVariablesManager.HOW_MANY_POINTS_MISSIONS_BETWEEN_LEVEL_21_AND_30, level);
+                assertEquals(SystemVariablesManager.POINTS_MISSIONS_BETWEEN_LEVEL_21_AND_30, level);
             else if (level >= 31 && level <= 40)
-                assertEquals(SystemVariablesManager.HOW_MANY_POINTS_MISSIONS_BETWEEN_LEVEL_31_AND_40, level);
+                assertEquals(SystemVariablesManager.POINTS_MISSIONS_BETWEEN_LEVEL_31_AND_40, level);
             else if (level >= 41 && level <= 50)
-                assertEquals(SystemVariablesManager.HOW_MANY_POINTS_MISSIONS_BETWEEN_LEVEL_41_AND_50, level);
+                assertEquals(SystemVariablesManager.POINTS_MISSIONS_BETWEEN_LEVEL_41_AND_50, level);
             else if (level >= 51 && level <= 60)
-                assertEquals(SystemVariablesManager.HOW_MANY_POINTS_MISSIONS_BETWEEN_LEVEL_51_AND_60, level);
+                assertEquals(SystemVariablesManager.POINTS_MISSIONS_BETWEEN_LEVEL_51_AND_60, level);
             else if (level >= 61 && level <= 70)
-                assertEquals(SystemVariablesManager.HOW_MANY_POINTS_MISSIONS_BETWEEN_LEVEL_61_AND_70, level);
+                assertEquals(SystemVariablesManager.POINTS_MISSIONS_BETWEEN_LEVEL_61_AND_70, level);
             else if (level >= 71 && level <= 80)
-                assertEquals(SystemVariablesManager.HOW_MANY_POINTS_MISSIONS_BETWEEN_LEVEL_71_AND_80, level);
+                assertEquals(SystemVariablesManager.POINTS_MISSIONS_BETWEEN_LEVEL_71_AND_80, level);
             else if (level >= 81 && level <= 90)
-                assertEquals(SystemVariablesManager.HOW_MANY_POINTS_MISSIONS_BETWEEN_LEVEL_81_AND_90, level);
+                assertEquals(SystemVariablesManager.POINTS_MISSIONS_BETWEEN_LEVEL_81_AND_90, level);
             else if (level >= 91 && level <= 100)
-                assertEquals(SystemVariablesManager.HOW_MANY_POINTS_MISSIONS_BETWEEN_LEVEL_91_AND_100, level);
+                assertEquals(SystemVariablesManager.POINTS_MISSIONS_BETWEEN_LEVEL_91_AND_100, level);
         });
     }
 }
