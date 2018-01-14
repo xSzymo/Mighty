4a347ccd492b0b42ad876198f9867c800d1a134a
xSzymo <szymekssss@wp.pl>
2018-01-14 09:23:08 +0100

    T: 53 - add test which check delete work can't delete champion

--- src/test/java/integration/data/services/WorkServiceTest.java
+++ src/test/java/integration/data/services/WorkServiceTest.java
@@ -1,6 +1,8 @@
 package integration.data.services;
 
+import game.mightywarriors.data.services.ChampionService;
 import game.mightywarriors.data.services.WorkService;
+import game.mightywarriors.data.tables.Champion;
 import game.mightywarriors.data.tables.Work;
 import integration.config.IntegrationTestsConfig;
 import org.junit.After;
@@ -16,105 +18,128 @@ import static org.junit.Assert.assertNull;
 public class WorkServiceTest extends IntegrationTestsConfig {
     @Autowired
     private WorkService objectUnderTest;
+    @Autowired
+    private ChampionService championService;
 
-    private LinkedList<Work> rankings;
+    private LinkedList<Work> works;
 
 
     @Before
     public void beforeEachTest() {
-        rankings = new LinkedList<>();
-        rankings.add(new Work("admin"));
-        rankings.add(new Work("user"));
-        rankings.add(new Work("other"));
+        works = new LinkedList<>();
+        works.add(new Work("admin"));
+        works.add(new Work("user"));
+        works.add(new Work("other"));
     }
 
     @After
     public void afterEachTest() {
-        rankings.forEach(objectUnderTest::delete);
+        works.forEach(objectUnderTest::delete);
     }
 
     @Test
     public void save() {
-        objectUnderTest.save(rankings.getFirst());
+        objectUnderTest.save(works.getFirst());
 
-        assertNotNull(objectUnderTest.findOne(rankings.getFirst()));
+        assertNotNull(objectUnderTest.findOne(works.getFirst()));
     }
 
     @Test
     public void saveCollection() {
-        objectUnderTest.save(rankings);
+        objectUnderTest.save(works);
 
-        assertNotNull(objectUnderTest.findOne(rankings.get(0)));
-        assertNotNull(objectUnderTest.findOne(rankings.get(1)));
-        assertNotNull(objectUnderTest.findOne(rankings.get(2)));
+        assertNotNull(objectUnderTest.findOne(works.get(0)));
+        assertNotNull(objectUnderTest.findOne(works.get(1)));
+        assertNotNull(objectUnderTest.findOne(works.get(2)));
     }
 
     @Test
     public void findOne() {
-        objectUnderTest.save(rankings.getFirst());
+        objectUnderTest.save(works.getFirst());
 
-        assertNotNull(objectUnderTest.findOne(rankings.getFirst()));
+        assertNotNull(objectUnderTest.findOne(works.getFirst()));
     }
 
     @Test
     public void findOne1() {
-        objectUnderTest.save(rankings.getFirst());
+        objectUnderTest.save(works.getFirst());
 
-        assertNotNull(objectUnderTest.findOne(rankings.getFirst().getNickname()));
+        assertNotNull(objectUnderTest.findOne(works.getFirst().getNickname()));
     }
 
     @Test
     public void findAll() {
-        objectUnderTest.save(rankings);
+        objectUnderTest.save(works);
 
-        assertNotNull(objectUnderTest.findAll().stream().filter(x -> x.getNickname().equals(rankings.get(0).getNickname())).findFirst().get());
-        assertNotNull(objectUnderTest.findAll().stream().filter(x -> x.getNickname().equals(rankings.get(1).getNickname())).findFirst().get());
-        assertNotNull(objectUnderTest.findAll().stream().filter(x -> x.getNickname().equals(rankings.get(2).getNickname())).findFirst().get());
+        assertNotNull(objectUnderTest.findAll().stream().filter(x -> x.getNickname().equals(works.get(0).getNickname())).findFirst().get());
+        assertNotNull(objectUnderTest.findAll().stream().filter(x -> x.getNickname().equals(works.get(1).getNickname())).findFirst().get());
+        assertNotNull(objectUnderTest.findAll().stream().filter(x -> x.getNickname().equals(works.get(2).getNickname())).findFirst().get());
     }
 
     @Test
     public void delete() {
-        objectUnderTest.save(rankings.getFirst());
+        objectUnderTest.save(works.getFirst());
 
-        Work one = objectUnderTest.findOne(rankings.getFirst());
+        Work one = objectUnderTest.findOne(works.getFirst());
 
         assertNotNull(one);
 
-        objectUnderTest.delete(rankings.getFirst());
+        objectUnderTest.delete(works.getFirst());
 
-        one = objectUnderTest.findOne(rankings.getFirst());
+        one = objectUnderTest.findOne(works.getFirst());
         assertNull(one);
-        rankings.clear();
+        works.clear();
     }
 
     @Test
     public void delete1() {
-        objectUnderTest.save(rankings.getFirst());
+        objectUnderTest.save(works.getFirst());
 
-        Work one = objectUnderTest.findOne(rankings.getFirst());
+        Work one = objectUnderTest.findOne(works.getFirst());
 
         assertNotNull(one);
 
-        objectUnderTest.delete(rankings.getFirst().getId());
+        objectUnderTest.delete(works.getFirst().getId());
 
-        one = objectUnderTest.findOne(rankings.getFirst());
+        one = objectUnderTest.findOne(works.getFirst());
         assertNull(one);
-        rankings.clear();
+        works.clear();
     }
 
     @Test
     public void delete2() {
-        objectUnderTest.save(rankings);
+        objectUnderTest.save(works);
 
-        assertNotNull(objectUnderTest.findOne(rankings.get(0)));
-        assertNotNull(objectUnderTest.findOne(rankings.get(1)));
-        assertNotNull(objectUnderTest.findOne(rankings.get(2)));
+        assertNotNull(objectUnderTest.findOne(works.get(0)));
+        assertNotNull(objectUnderTest.findOne(works.get(1)));
+        assertNotNull(objectUnderTest.findOne(works.get(2)));
 
-        objectUnderTest.delete(rankings);
+        objectUnderTest.delete(works);
 
-        assertNull(objectUnderTest.findOne(rankings.get(0)));
-        assertNull(objectUnderTest.findOne(rankings.get(1)));
-        assertNull(objectUnderTest.findOne(rankings.get(2)));
-        rankings.clear();
+        assertNull(objectUnderTest.findOne(works.get(0)));
+        assertNull(objectUnderTest.findOne(works.get(1)));
+        assertNull(objectUnderTest.findOne(works.get(2)));
+        works.clear();
+    }
+
+    @Test
+    public void deleteFromChampion() {
+        Work first = works.getFirst();
+        Champion champion = new Champion();
+        first.setChampion(champion);
+
+        championService.save(champion);
+        objectUnderTest.save(works);
+
+        assertNotNull(objectUnderTest.findOne(works.getFirst()));
+        assertNotNull(objectUnderTest.findOne(works.getFirst()).getChampion());
+        assertNotNull(championService.findOne(champion.getId()));
+
+        objectUnderTest.delete(works.getFirst());
+
+        assertNull(objectUnderTest.findOne(works.getFirst()));
+        assertNull(objectUnderTest.findOne(works.getFirst()));
+        assertNotNull(championService.findOne(champion.getId()));
+        works.clear();
     }
 }
