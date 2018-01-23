ae1d2ea68308440c30fbbf96a248c55f37f2d33b
xSzymo <szymekssss@wp.pl>
2018-01-23 22:08:43 +0100

    T: 44 - add functionality which adds items for all champions instead of one with highest level

--- src/main/java/game/mightywarriors/services/background/tasks/ItemDrawer.java
+++ src/main/java/game/mightywarriors/services/background/tasks/ItemDrawer.java
@@ -3,6 +3,7 @@ package game.mightywarriors.services.background.tasks;
 import game.mightywarriors.configuration.system.variables.SystemVariablesManager;
 import game.mightywarriors.data.services.ItemService;
 import game.mightywarriors.data.services.UserService;
+import game.mightywarriors.data.tables.Champion;
 import game.mightywarriors.data.tables.Item;
 import game.mightywarriors.data.tables.User;
 import game.mightywarriors.other.enums.WeaponType;
@@ -22,6 +23,7 @@ public class ItemDrawer {
     @Autowired
     private ItemService itemService;
     private Map map;
+    private static long howManyItemsForOneChampion = 10;
 
     public ItemDrawer() {
         rand = new Random();
@@ -50,52 +52,59 @@ public class ItemDrawer {
             throw new RuntimeException("restart system");
 
         draw(users, items, true);
-        draw(users, items, false);
 
         userService.save(users);
     }
 
     private Set<User> draw(Set<User> users, Set<Item> items, boolean clear) {
-        users.forEach(user -> {
-            if (user.getChampions().size() == 0)
-                throw new RuntimeException("restart system");
-
-            List<Item> oldItemsInShop = new LinkedList<>(user.getShop().getItems());
-            if (oldItemsInShop.size() == 0)
-                oldItemsInShop = new LinkedList<>();
+        Iterator<User> iterator = users.iterator();
+        while (iterator.hasNext()) {
+            User user = iterator.next();
 
             if (clear)
                 user.getShop().getItems().clear();
 
-            List<Item> itemsForSpecificLevel = new LinkedList<>(getAllItemsForSpecificLevel(items, user.getUserChampiongHighestLevel()));
-            while (user.getShop().getItems().size() < 10 && itemsForSpecificLevel.size() > 0) {
-                Item item = null;
-                try {
-                    item = itemsForSpecificLevel.get(rand.nextInt(itemsForSpecificLevel.size() > 0 ? itemsForSpecificLevel.size() : 1));
-
-                    if (item.getLevel() > user.getUserChampiongHighestLevel())
-                        continue;
-
-                    user = addItemForSpecificType(oldItemsInShop, user, item, WeaponType.WEAPON);
-                    user = addItemForSpecificType(oldItemsInShop, user, item, WeaponType.ARMOR);
-                    user = addItemForSpecificType(oldItemsInShop, user, item, WeaponType.BOOTS);
-                    user = addItemForSpecificType(oldItemsInShop, user, item, WeaponType.BRACELET);
-                    user = addItemForSpecificType(oldItemsInShop, user, item, WeaponType.GLOVES);
-                    user = addItemForSpecificType(oldItemsInShop, user, item, WeaponType.HELMET);
-                    user = addItemForSpecificType(oldItemsInShop, user, item, WeaponType.LEGS);
-                    user = addItemForSpecificType(oldItemsInShop, user, item, WeaponType.NECKLACE);
-                    user = addItemForSpecificType(oldItemsInShop, user, item, WeaponType.OFFHAND);
-                    user = addItemForSpecificType(oldItemsInShop, user, item, WeaponType.RING);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                } finally {
-                    itemsForSpecificLevel.remove(item);
-                }
+            Iterator<Champion> champions = user.getChampions().iterator();
+            for (int i = 1; i <= user.getChampions().size(); i++) {
+                Champion champion = champions.next();
+                draw(user, items, champion.getLevel(), i * howManyItemsForOneChampion, (i * howManyItemsForOneChampion) - howManyItemsForOneChampion, i);
             }
-        });
-
+        }
         return users;
     }
+
+    private void draw(User user, Set<Item> items, long limitedLevel, long maxItems, long minItems, int times) {
+        if (user.getChampions().size() == 0)
+            throw new RuntimeException("restart system");
+
+        List<Item> oldItemsInShop = new LinkedList<>(user.getShop().getItems());
+        if (oldItemsInShop.size() == 0)
+            oldItemsInShop = new LinkedList<>();
+
+        List<Item> itemsForSpecificLevel = new LinkedList<>(getAllItemsForSpecificLevel(items, limitedLevel));
+        while (user.getShop().getItems().size() < maxItems && itemsForSpecificLevel.size() > minItems) {
+            Item item = null;
+            try {
+                item = itemsForSpecificLevel.get(rand.nextInt(itemsForSpecificLevel.size() > 0 ? itemsForSpecificLevel.size() : 1));
+
+                user = addItemForSpecificType(oldItemsInShop, user, item, times, WeaponType.WEAPON);
+                user = addItemForSpecificType(oldItemsInShop, user, item, times, WeaponType.ARMOR);
+                user = addItemForSpecificType(oldItemsInShop, user, item, times, WeaponType.BOOTS);
+                user = addItemForSpecificType(oldItemsInShop, user, item, times, WeaponType.BRACELET);
+                user = addItemForSpecificType(oldItemsInShop, user, item, times, WeaponType.GLOVES);
+                user = addItemForSpecificType(oldItemsInShop, user, item, times, WeaponType.HELMET);
+                user = addItemForSpecificType(oldItemsInShop, user, item, times, WeaponType.LEGS);
+                user = addItemForSpecificType(oldItemsInShop, user, item, times, WeaponType.NECKLACE);
+                user = addItemForSpecificType(oldItemsInShop, user, item, times, WeaponType.OFFHAND);
+                user = addItemForSpecificType(oldItemsInShop, user, item, times, WeaponType.RING);
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                itemsForSpecificLevel.remove(item);
+            }
+        }
+    }
+
 
     private List<Item> getAllItemsForSpecificLevel(Set<Item> items, long level) {
         if (map.get(level) == null)
@@ -104,10 +113,14 @@ public class ItemDrawer {
         return (List<Item>) map.get(level);
     }
 
-    private User addItemForSpecificType(List<Item> oldItemsInShop, User user, Item item, WeaponType weaponType) {
+    private User addItemForSpecificType(List<Item> oldItemsInShop, User user, Item item, int times, WeaponType weaponType) {
+        int counter = 0;
         for (Item x : user.getShop().getItems())
             if (x.getTypeOfWeapon().getType().equals(weaponType.getType()))
-                return user;
+                counter++;
+
+        if (counter >= times)
+            return user;
 
         if (item != null)
             if (!oldItemsInShop.contains(item))
