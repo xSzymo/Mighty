32ac8f4110205f529d0a2fcd2ddb065ed4aa6af4
xSzymo <szymekssss@wp.pl>
2018-01-24 12:09:46 +0100

    T: 58 - add new tests in ShopManagerTest

--- src/test/java/advanced/integration/services/bookmarks/shop/ShopManagerTest.java
+++ src/test/java/advanced/integration/services/bookmarks/shop/ShopManagerTest.java
@@ -1,9 +1,11 @@
 package advanced.integration.services.bookmarks.shop;
 
 import advanced.integration.config.AuthorizationConfiguration;
+import game.mightywarriors.data.services.ItemService;
 import game.mightywarriors.data.services.UserService;
 import game.mightywarriors.data.tables.Item;
 import game.mightywarriors.data.tables.User;
+import game.mightywarriors.other.exceptions.NotEnoughGoldException;
 import game.mightywarriors.services.security.UsersRetriever;
 import game.mightywarriors.web.json.objects.bookmarks.tavern.ShopInformer;
 import game.mightywarriors.web.rest.bookmarks.shop.ShopController;
@@ -21,27 +23,34 @@ public class ShopManagerTest extends AuthorizationConfiguration {
     @Autowired
     private UserService userService;
     @Autowired
+    private ItemService itemService;
+    @Autowired
     private UsersRetriever usersRetriever;
 
     private User user;
     private Item item;
+    private Item item1;
     private ShopInformer informer;
 
     @Before
     public void setUp() throws Exception {
         informer = new ShopInformer();
         user = usersRetriever.retrieveUser(token);
-        user.addGold(new BigDecimal("1000"));
         item = user.getShop().getItems().iterator().next();
+        item.setGold(new BigDecimal("10"));
 
         informer.itemId = item.getId();
         userService.save(user);
+        item1 = new Item();
+        itemService.save(item);
     }
 
     @Test
     public void buyItem() throws Exception {
+        user.addGold(new BigDecimal("1000"));
         BigDecimal itemGold = item.getGold();
         BigDecimal userGold = user.getGold();
+        userService.save(user);
 
         objectUnderTest.buyItem(token, informer);
 
@@ -49,5 +58,20 @@ public class ShopManagerTest extends AuthorizationConfiguration {
         assertEquals(userGold.subtract(itemGold), user.getGold());
         assertFalse(user.getShop().getItems().stream().anyMatch(x -> x.getId().equals(item.getId())));
         assertTrue(user.getInventory().getItems().stream().anyMatch(x -> x.getId().equals(item.getId())));
+    }
+
+    @Test(expected = NotEnoughGoldException.class)
+    public void buyItem_withNotEnoughMoney() throws Exception {
+        user.setGold(new BigDecimal("0"));
+        userService.save(user);
+
+        objectUnderTest.buyItem(token, informer);
+    }
+
+    @Test(expected = Exception.class)
+    public void buyItem_withWrongItem() throws Exception {
+        informer.itemId = item1.getId();
+
+        objectUnderTest.buyItem(token, informer);
     }
 }
