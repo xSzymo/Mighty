c2d97d807718902312b4a08ef70664419eaa7608
xSzymo <szymekssss@wp.pl>
2018-01-24 23:43:45 +0100

    T: 59 - create place informer for json representation informer ItemManager

---
+++ src/main/java/game/mightywarriors/web/json/objects/bookmarks/tavern/PlaceInformer.java
@@ -0,0 +1,16 @@
+package game.mightywarriors.web.json.objects.bookmarks.tavern;
+
+public class PlaceInformer {
+
+    /**
+     * Profile
+     * itemId : item id;
+     */
+    public long itemId;
+
+    /**
+     * championId : champion id
+     */
+    public long championId;
+
+}
