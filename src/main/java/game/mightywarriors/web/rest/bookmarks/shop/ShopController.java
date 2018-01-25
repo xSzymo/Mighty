7888e7b8e20226b124ddddb8c81ace0390bbd2c2
xSzymo <szymekssss@wp.pl>
2018-01-25 15:53:34 +0100

    T: 61 - add sell item end point

--- src/main/java/game/mightywarriors/web/rest/bookmarks/shop/ShopController.java
+++ src/main/java/game/mightywarriors/web/rest/bookmarks/shop/ShopController.java
@@ -1,13 +1,8 @@
 package game.mightywarriors.web.rest.bookmarks.shop;
 
 import game.mightywarriors.configuration.system.variables.SystemVariablesManager;
-import game.mightywarriors.data.tables.MissionFight;
 import game.mightywarriors.services.bookmarks.shop.ShopManager;
-import game.mightywarriors.services.bookmarks.tavern.MissionFightChecker;
-import game.mightywarriors.services.bookmarks.tavern.TavernManager;
-import game.mightywarriors.web.json.objects.bookmarks.tavern.Informer;
 import game.mightywarriors.web.json.objects.bookmarks.tavern.ShopInformer;
-import game.mightywarriors.web.json.objects.fights.FightResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,5 +18,12 @@ public class ShopController {
     public void buyItem(@RequestHeader(value = SystemVariablesManager.NAME_OF_JWT_HEADER_TOKEN) String authorization, @RequestBody ShopInformer informer) throws Exception {
 
         manager.buyItem(authorization, informer);
+    }
+
+
+    @PostMapping("secure/shop/sell")
+    public void sellItem(@RequestHeader(value = SystemVariablesManager.NAME_OF_JWT_HEADER_TOKEN) String authorization, @RequestBody ShopInformer informer) throws Exception {
+
+        manager.sellItem(authorization, informer);
     }
 }
