a38f290c97ccff273207b40b7d5b5a33e27473f8
xSzymo <szymekssss@wp.pl>
2018-01-27 01:26:44 +0100

    T: 34 - created message repository

---
+++ src/main/java/game/mightywarriors/data/repositories/MessageRepository.java
@@ -0,0 +1,19 @@
+package game.mightywarriors.data.repositories;
+
+import game.mightywarriors.data.tables.Message;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
+import java.util.HashSet;
+import java.util.LinkedList;
+
+@Repository
+@Transactional
+public interface MessageRepository extends CrudRepository<Message, Long> {
+    Message findById(long id);
+
+    LinkedList<Message> findAll();
+
+    void deleteById(Long id);
+}
