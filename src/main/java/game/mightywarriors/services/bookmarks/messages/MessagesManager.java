ef1e8c2a3d72ca9a057bfed80cc3ac0c93c6e38a
xSzymo <szymekssss@wp.pl>
2018-01-29 02:25:45 +0100

    T: 34 - created MessageManager

---
+++ src/main/java/game/mightywarriors/services/bookmarks/messages/MessagesManager.java
@@ -0,0 +1,84 @@
+package game.mightywarriors.services.bookmarks.messages;
+
+import game.mightywarriors.configuration.system.variables.SystemVariablesManager;
+import game.mightywarriors.data.services.ChatService;
+import game.mightywarriors.data.services.MessageService;
+import game.mightywarriors.data.tables.Chat;
+import game.mightywarriors.data.tables.Message;
+import game.mightywarriors.data.tables.User;
+import game.mightywarriors.other.exceptions.NoAccessException;
+import game.mightywarriors.other.exceptions.NotFoundException;
+import game.mightywarriors.services.bookmarks.utilities.MessageHelper;
+import game.mightywarriors.services.security.UsersRetriever;
+import game.mightywarriors.web.json.objects.bookmarks.MessageInformer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MessagesManager {
+    @Autowired
+    private UsersRetriever retriever;
+    @Autowired
+    private ChatService chatService;
+    @Autowired
+    private MessageService messageService;
+    @Autowired
+    private MessageHelper helper;
+
+    public void addMessage(String authorization, MessageInformer informer) throws Exception {
+        User user = retriever.retrieveUser(authorization);
+        Chat chat = chatService.find(informer.chatId);
+
+        throwExceptionIf_MessageIsNotPresent(informer);
+        throwExceptionIf_ChatIsNotPresent(chat);
+        throwExceptionIf_UserHaveNotAccessToChat(user, chat);
+
+        chat = addMessageToChat(chat, informer.message, user.getLogin());
+        chatService.save(chat);
+    }
+
+    public void deleteMessage(String authorization, MessageInformer informer) throws Exception {
+        User user = retriever.retrieveUser(authorization);
+        Chat chat = chatService.find(informer.chatId);
+        Message message = messageService.find(informer.messageId);
+
+        throwExceptionIf_MessageIsNotPresent(message);
+        throwExceptionIf_UserIsNotOwnerOfMessageOrHaveNotRights(user, chat, message);
+
+        message.setMessage(SystemVariablesManager.INFORMATION_OF_DELETED_MESSAGE);
+        messageService.save(message);
+    }
+
+    private Chat addMessageToChat(Chat chat, String myMessage, String login) {
+        Message message = new Message(myMessage, login);
+        message.setNumber(chat.getMessages().size());
+        chat.addMessage(message);
+
+        return chat;
+    }
+
+    private void throwExceptionIf_MessageIsNotPresent(MessageInformer informer) throws Exception {
+        if (informer.message == null)
+            throw new NotFoundException("Message is not present");
+    }
+
+    private void throwExceptionIf_MessageIsNotPresent(Message message) throws Exception {
+        if (message == null)
+            throw new NotFoundException("Message not found");
+    }
+
+    private void throwExceptionIf_ChatIsNotPresent(Chat chat) throws Exception {
+        if (chat == null)
+            throw new NotFoundException("Chat not found");
+    }
+
+    private void throwExceptionIf_UserHaveNotAccessToChat(User user, Chat chat) throws Exception {
+        if (!user.getChats().contains(chat))
+            throw new NoAccessException("User have not access to chat");
+    }
+
+    private void throwExceptionIf_UserIsNotOwnerOfMessageOrHaveNotRights(User user, Chat chat, Message message) throws Exception {
+        if (!message.getUserLogin().equals(user.getLogin()) && chat.getAdmins().stream().noneMatch(x -> x.getLogin().equals(user.getLogin())))
+            throw new NoAccessException("You haven't permission");
+    }
+}
