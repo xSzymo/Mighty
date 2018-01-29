41cdae2b94cd4db89ec8bbf454ed6dd68bca2825
xSzymo <szymekssss@wp.pl>
2018-01-29 02:24:01 +0100

    T: 0 - clean up

--- src/main/java/game/mightywarriors/data/services/MessageService.java
+++ src/main/java/game/mightywarriors/data/services/MessageService.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
-import java.util.*;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
 
 @Service
 @Transactional
