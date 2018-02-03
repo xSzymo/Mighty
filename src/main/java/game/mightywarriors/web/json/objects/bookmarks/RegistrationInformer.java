8cdb9349e02953596d8f3aba4eb55493a21eda5a
xSzymo <szymekssss@wp.pl>
2018-02-03 16:54:16 +0100

    T: 15 - add RegistrationInformer

---
+++ src/main/java/game/mightywarriors/web/json/objects/bookmarks/RegistrationInformer.java
@@ -0,0 +1,16 @@
+package game.mightywarriors.web.json.objects.bookmarks;
+
+public class RegistrationInformer {
+
+    /**
+     * Registration
+     * login : login
+     * password : password
+     * email : email
+     * code : code from email to enable account
+     */
+    public String login;
+    public String password;
+    public String email;
+    public String code;
+}
