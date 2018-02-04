5b12c86647473768c40bcdbe10334a9d94d26fff
xSzymo <szymekssss@wp.pl>
2018-02-05 00:31:39 +0100

    T: 82 - add remind login/password end points & json representation of RemindInformer

---
+++ src/main/java/game/mightywarriors/web/json/objects/bookmarks/RemindInformer.java
@@ -0,0 +1,10 @@
+package game.mightywarriors.web.json.objects.bookmarks;
+
+public class RemindInformer {
+
+    /**
+     * Reminder
+     * email: email to send
+     */
+    public String email;
+}
