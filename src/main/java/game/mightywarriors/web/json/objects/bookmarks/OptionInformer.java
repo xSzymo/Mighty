b66900cb1b9e6b42e1c0b955ecf1b0e7eea4c1e9
xSzymo <szymekssss@wp.pl>
2018-02-04 23:23:35 +0100

    T 75 - add OptionInformer

---
+++ src/main/java/game/mightywarriors/web/json/objects/bookmarks/OptionInformer.java
@@ -0,0 +1,16 @@
+package game.mightywarriors.web.json.objects.bookmarks;
+
+public class OptionInformer {
+
+    /**
+     * Options
+     * nick : new nick
+     * password : new password
+     * email : new email
+     * code : code to authorize operation
+     */
+    public String login;
+    public String password;
+    public String email;
+    public String code;
+}
