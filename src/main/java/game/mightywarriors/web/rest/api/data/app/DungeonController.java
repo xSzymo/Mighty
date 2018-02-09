4d73ac2175d8fe2fa10a52e34195db5a985fc8a9
xSzymo <szymekssss@wp.pl>
2018-02-10 00:39:46 +0100

    T: 80 - add dungeon end points

---
+++ src/main/java/game/mightywarriors/web/rest/api/data/app/DungeonController.java
@@ -0,0 +1,22 @@
+package game.mightywarriors.web.rest.api.data.app;
+
+
+import game.mightywarriors.data.services.DungeonService;
+import game.mightywarriors.data.tables.Dungeon;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashSet;
+
+@RestController("dungeonsController")
+public class DungeonController {
+    @Autowired
+    private DungeonService dungeonService;
+
+    @GetMapping("dungeons")
+    public HashSet<Dungeon> getAllDungeons() {
+
+        return dungeonService.findAll();
+    }
+}
