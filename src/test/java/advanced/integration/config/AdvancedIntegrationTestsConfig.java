7d656eb84543a87bc3383eb7f0a60933b0b0d065
xSzymo <szymekssss@wp.pl>
2018-02-10 00:30:26 +0100

    T: 80 - add sample dungeons for advanced.integration tests

--- src/test/java/advanced/integration/config/AdvancedIntegrationTestsConfig.java
+++ src/test/java/advanced/integration/config/AdvancedIntegrationTestsConfig.java
@@ -2,15 +2,12 @@ package advanced.integration.config;
 
 import game.mightywarriors.MightyWarriorsApplication;
 import game.mightywarriors.configuration.system.variables.SystemVariablesManager;
+import game.mightywarriors.data.services.DungeonService;
 import game.mightywarriors.data.services.ItemService;
 import game.mightywarriors.data.services.MissionService;
-import game.mightywarriors.data.tables.Item;
-import game.mightywarriors.data.tables.Mission;
-import game.mightywarriors.data.tables.Monster;
-import game.mightywarriors.data.tables.Statistic;
+import game.mightywarriors.data.tables.*;
 import game.mightywarriors.other.enums.ItemType;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,6 +15,8 @@ import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.math.BigDecimal;
+import java.util.Collections;
+import java.util.HashSet;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = MightyWarriorsApplication.class, webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
@@ -25,9 +24,8 @@ import java.math.BigDecimal;
 public abstract class AdvancedIntegrationTestsConfig {
     private static ItemService itemService;
     private static MissionService missionService;
+    private static DungeonService dungeonService;
     private static boolean run = true;
-
-
 
     @Before
     public void createMonstersAndItems() {
@@ -56,6 +54,25 @@ public abstract class AdvancedIntegrationTestsConfig {
         itemService.save(new Item("name" + 8, ItemType.NECKLACE, new Statistic(1, 1, 1, 1, 1, 1), 1));
         itemService.save(new Item("name" + 9, ItemType.OFFHAND, new Statistic(1, 1, 1, 1, 1, 1), 1));
         itemService.save(new Item("name" + 10, ItemType.RING, new Statistic(1, 1, 1, 1, 1, 1), 1));
+
+
+        Dungeon dungeon = new Dungeon();
+        dungeon.setNumber(1);
+        dungeon.setName("dungeon 1");
+        for(int i = 1; i <= SystemVariablesManager.MAX_FLOORS_PER_DUNGEON; i++) {
+            monster = new Monster(new Statistic(1 + i, 1 + i, 1, 1, 1, 1));
+            dungeon.getFloors().add(new Floor(5 + i, 1, i, new BigDecimal("5"), new HashSet<>(Collections.singleton(monster)), itemService.find(i), new Image()));
+        }
+        dungeonService.save(dungeon);
+
+        dungeon = new Dungeon();
+        dungeon.setNumber(2);
+        dungeon.setName("dungeon 2");
+        for(int i = 1; i <= SystemVariablesManager.MAX_FLOORS_PER_DUNGEON; i++) {
+            monster = new Monster(new Statistic(1 + i, 1, 1 + i, 1, 1, 1));
+            dungeon.getFloors().add(new Floor(20 + i, 1, i, new BigDecimal("10"), new HashSet<>(Collections.singleton(monster)), null, new Image()));
+        }
+        dungeonService.save(dungeon);
     }
 
     @Autowired
@@ -66,5 +83,10 @@ public abstract class AdvancedIntegrationTestsConfig {
     @Autowired
     public void setMissionService(MissionService missionService) {
         AdvancedIntegrationTestsConfig.missionService = missionService;
+    }
+
+    @Autowired
+    public void setDungeonService(DungeonService dungeonService) {
+        AdvancedIntegrationTestsConfig.dungeonService = dungeonService;
     }
 }
