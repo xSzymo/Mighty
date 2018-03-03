54f0bef3676bac87f732542550aa135b4d40c35e
xSzymo <szymekssss@wp.pl>
2018-03-04 00:20:18 +0100

    T: 103 - add end points to buy and check information about buy champions

--- src/main/java/game/mightywarriors/web/rest/mighty/bookmarks/tavern/TavernController.java
+++ src/main/java/game/mightywarriors/web/rest/mighty/bookmarks/tavern/TavernController.java
@@ -1,9 +1,10 @@
 package game.mightywarriors.web.rest.mighty.bookmarks.tavern;
 
 import game.mightywarriors.configuration.system.variables.SystemVariablesManager;
+import game.mightywarriors.services.bookmarks.tavern.ChampionTavern;
 import game.mightywarriors.services.bookmarks.tavern.MissionFightChecker;
 import game.mightywarriors.services.bookmarks.tavern.TavernManager;
-import game.mightywarriors.web.json.objects.bookmarks.Informer;
+import game.mightywarriors.web.json.objects.bookmarks.ChampionBuyInformer;
 import game.mightywarriors.web.json.objects.bookmarks.MissionFightInformer;
 import game.mightywarriors.web.json.objects.bookmarks.TavernInformer;
 import game.mightywarriors.web.json.objects.fights.FightResult;
@@ -17,6 +18,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class TavernController {
     @Autowired
     private TavernManager tavernManager;
+    @Autowired
+    private ChampionTavern buyer;
     @Autowired
     private MissionFightChecker missionFightChecker;
 
@@ -36,5 +39,17 @@ public class TavernController {
     public long checkMissionFightBlockTime(@RequestHeader(value = SystemVariablesManager.NAME_OF_JWT_HEADER_TOKEN) String authorization, @RequestBody MissionFightInformer informer) throws Exception {
 
         return missionFightChecker.checkLeftTimeForMissionFight(authorization, informer);
+    }
+
+    @PostMapping("secure/tavern/champion/buy")
+    public void buyChampion(@RequestHeader(value = SystemVariablesManager.NAME_OF_JWT_HEADER_TOKEN) String authorization) throws Exception {
+
+        buyer.buyChampion(authorization);
+    }
+
+    @PostMapping("secure/tavern/champion/buy/information")
+    public ChampionBuyInformer getInformationForBuyChampion(@RequestHeader(value = SystemVariablesManager.NAME_OF_JWT_HEADER_TOKEN) String authorization) throws Exception {
+
+        return buyer.getInformationForBuyChampion(authorization);
     }
 }
