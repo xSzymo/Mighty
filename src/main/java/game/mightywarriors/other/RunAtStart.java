74bcdf7a4810f22a54baaaa05b570c5570ca4091
xSzymo <szymeklaciak@gmail.com>
2018-03-18 11:35:29 +0100

    T: 120 - add sample data in RAS

--- src/main/java/game/mightywarriors/other/RunAtStart.java
+++ src/main/java/game/mightywarriors/other/RunAtStart.java
@@ -1,12 +1,11 @@
 package game.mightywarriors.other;
 
 import game.mightywarriors.configuration.system.variables.SystemVariablesManager;
-import game.mightywarriors.data.services.ItemService;
-import game.mightywarriors.data.services.RoleService;
-import game.mightywarriors.data.services.UserService;
+import game.mightywarriors.data.services.*;
 import game.mightywarriors.data.tables.*;
-import game.mightywarriors.other.enums.ItemType;
-import game.mightywarriors.other.enums.Kingdom;
+import game.mightywarriors.other.enums.*;
+import game.mightywarriors.services.background.tasks.DivisionAssigner;
+import game.mightywarriors.web.rest.mighty.bookmarks.guild.GuildController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -15,6 +14,8 @@ import java.math.BigDecimal;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Component
 public class RunAtStart {
@@ -24,9 +25,22 @@ public class RunAtStart {
     private ItemService itemService;
     @Autowired
     private RoleService roleService;
+    @Autowired
+    private DivisionAssigner divisionAssigner;
+    @Autowired
+    private DivisionService divisionService;
+    @Autowired
+    private DungeonService dungeonService;
+    @Autowired
+    private MissionService missionService;
+    @Autowired
+    private GuildController guildController;
+    @Autowired
+    private GuildService guildService;
 
     private HashSet<User> users = new HashSet<>();
     private LinkedList<Mission> missions = new LinkedList<>();
+    private LinkedList<Mission> missions1 = new LinkedList<>();
     private LinkedList<Champion> champions = new LinkedList<>();
     private LinkedList<Image> images = new LinkedList<>();
     private LinkedList<Statistic> statistics = new LinkedList<>();
@@ -34,6 +48,7 @@ public class RunAtStart {
     private LinkedList<Shop> shops = new LinkedList<>();
     private LinkedList<Item> items = new LinkedList<>();
     private LinkedList<Equipment> equipments = new LinkedList<>();
+    private LinkedList<Dungeon> dungeons = new LinkedList<>();
 
     @PostConstruct
     public void addExampleDataForTestAtStartApplication() throws Exception {
@@ -44,7 +59,6 @@ public class RunAtStart {
         Role user_role = roleService.find("user");
         roleService.save(admin_role);
         roleService.save(user_role);
-
 
         LinkedList<Item> myItems = new LinkedList<>();
         Statistic statistic;
@@ -114,10 +128,43 @@ public class RunAtStart {
                 missions.add(new Mission(1 + i, "", new BigDecimal("1"), monsters.get(i)).setImageLight(new Image("http://dclegends.wiki/images/thumb/0/0e/Alliance_Missions.jpg/300px-Alliance_Missions.jpg")).setDuration((10 + i * 3)));
         }
 
+        for (int i = 0; i < 30; i++) {
+            missions1.add(new Mission(30 + i, "", new BigDecimal("1"), new Monster(new Statistic(1, 1, 1, 1, 1, 1)).setLevel(i)).setImageLight(new Image("http://dclegends.wiki/images/thumb/0/0e/Alliance_Missions.jpg/300px-Alliance_Missions.jpg")).setDuration((5)));
+            missions1.add(new Mission(40 + i, "", new BigDecimal("10"), new Monster(new Statistic(1, 1, 1, 1, 1, 1)).setLevel(i)).setImageLight(new Image("http://dclegends.wiki/images/thumb/0/0e/Alliance_Missions.jpg/300px-Alliance_Missions.jpg")).setDuration((5)));
+            missions1.add(new Mission(50 + i, "", new BigDecimal("100"), new Monster(new Statistic(1, 1, 1, 1, 1, 1)).setLevel(i)).setImageLight(new Image("http://dclegends.wiki/images/thumb/0/0e/Alliance_Missions.jpg/300px-Alliance_Missions.jpg")).setDuration((5)));
+        }
+
+        if (SystemVariablesManager.ADD_SAMPLE_DUNGEONS)
+            for (int i = 0; i < 10; i++) {
+                HashSet floors = new HashSet();
+
+                //make it works
+                int a = 0;
+                HashSet<Monster> objects = new HashSet<>();
+                objects.add(monsters.get(i));
+                floors.add(new Floor(i * 5 + a++ + 1, i * 10 + a, a, new BigDecimal(Integer.toString(i * 100 + a)), Stream.of(new Monster(new Statistic(), null)).collect(Collectors.toSet()), items.get(i), null));
+                floors.add(new Floor(i * 5 + a++ + 2, i * 10 + a, a, new BigDecimal(Integer.toString(i * 100 + a)), Stream.of(new Monster(new Statistic(), null)).collect(Collectors.toSet()), items.get(i), null));
+                floors.add(new Floor(i * 5 + a++ + 3, i * 10 + a, a, new BigDecimal(Integer.toString(i * 100 + a)), Stream.of(new Monster(new Statistic(), null)).collect(Collectors.toSet()), items.get(i), null));
+                floors.add(new Floor(i * 5 + a++ + 4, i * 10 + a, a, new BigDecimal(Integer.toString(i * 100 + a)), Stream.of(new Monster(new Statistic(), null)).collect(Collectors.toSet()), items.get(i), null));
+                floors.add(new Floor(i * 5 + a++ + 5, i * 10 + a, a, new BigDecimal(Integer.toString(i * 100 + a)), Stream.of(new Monster(new Statistic(), null)).collect(Collectors.toSet()), items.get(i), null));
+                floors.add(new Floor(i * 5 + a++ + 6, i * 10 + a, a, new BigDecimal(Integer.toString(i * 100 + a)), Stream.of(new Monster(new Statistic(), null)).collect(Collectors.toSet()), items.get(i), null));
+                floors.add(new Floor(i * 5 + a++ + 7, i * 10 + a, a, new BigDecimal(Integer.toString(i * 100 + a)), Stream.of(new Monster(new Statistic(), null)).collect(Collectors.toSet()), items.get(i), null));
+                floors.add(new Floor(i * 5 + a++ + 8, i * 10 + a, a, new BigDecimal(Integer.toString(i * 100 + a)), Stream.of(new Monster(new Statistic(), null)).collect(Collectors.toSet()), items.get(i), null));
+                floors.add(new Floor(i * 5 + a++ + 9, i * 10 + a, a, new BigDecimal(Integer.toString(i * 100 + a)), Stream.of(new Monster(new Statistic(), null)).collect(Collectors.toSet()), items.get(i), null));
+                floors.add(new Floor(i * 5 + a++ + 10, i * 10 + a, a, new BigDecimal(Integer.toString(i * 100 + a)), Stream.of(new Monster(new Statistic(), null)).collect(Collectors.toSet()), items.get(i), null));
+
+                Dungeon dungeon = new Dungeon("dungeon " + i, i, new Image(), floors);
+                dungeonService.save(dungeon);
+                dungeons.add(dungeon);
+            }
+
         for (int i = 0; i < 12; i++) {
             statistics.add(new Statistic());
             images.add(new Image("http://beaver.blox.pl/resource/bober22.jpg"));
-            champions.add(new Champion(statistics.get(i), images.get(i), equipments.get(i)));
+            if (i > 6)
+                champions.add(new Champion(statistics.get(i), images.get(i), equipments.get(i)).setLevel(i * 2));
+            else
+                champions.add(new Champion(statistics.get(i), images.get(i), equipments.get(i)).setLevel(i * 4 + 1));
         }
 
         for (int a = 0, b = 0, i = 0; i < 4; i++) {
@@ -139,6 +186,8 @@ public class RunAtStart {
             user.setAccountEnabled(true);
             user.setAccountNonLocked(true);
             user.addGold(new BigDecimal("10000"));
+            if (SystemVariablesManager.ADD_SAMPLE_DUNGEONS)
+                user.setDungeon(new UserDungeon(dungeons.get(i)));
             users.add(user);
         }
         userService.save(users);
@@ -151,6 +200,49 @@ public class RunAtStart {
             next.getInventory().addItem(itemService.find(i + 1));
             next.getInventory().addItem(itemService.find(i + 2));
         }
+
         userService.save(users);
+
+        HashSet<User> all = userService.findAll();
+        all.forEach(x -> x.setDungeonPoints(10));
+        userService.save(all);
+
+        divisionAssigner.assignUsersDivisions();
+        Division division = divisionService.find(League.WOOD);
+        division.getUsers().add(userService.find("admin0"));
+        divisionService.save(division);
+
+        missionService.save(missions1);
+
+        user = userService.find("admin0");
+        Role role = roleService.find(GuildRole.OWNER.getRole());
+        role.getUsers().add(user);
+        roleService.save(role);
+
+        Chat chat = new Chat();
+        chat.getAdmins().add(new Admin(ChatRole.OWNER, user.getLogin()));
+        Guild guild = new Guild("test");
+        guild.setMinimumLevel(1);
+        guild.addUser(user);
+        guild.setChat(chat);
+        guildService.save(guild);
+        user.setRole(roleService.find(role));
+        user.setGuild(guild);
+        user.addChat(chat);
+        userService.save(user);
+
+        User invitedUser = userService.find("user1");
+        guild = user.getGuild();
+        invitedUser.setGuild(guild);
+        invitedUser.getChats().add(user.getGuild().getChat());
+        guild.addUser(invitedUser);
+        userService.save(invitedUser);
+
+        invitedUser = userService.find("user3");
+        guild = user.getGuild();
+        invitedUser.setGuild(guild);
+        invitedUser.getChats().add(user.getGuild().getChat());
+        guild.addUser(invitedUser);
+        userService.save(invitedUser);
     }
 }
