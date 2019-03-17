f9ad425aa2054815da894c3c99df6c9b3d5879db
xSzymo <szymeklaciak@gmail.com>
2019-03-17 13:22:56 +0100

    add swagger

--- src/main/java/game/mightywarriors/configuration/system/system/SystemBeansManager.java
+++ src/main/java/game/mightywarriors/configuration/system/system/SystemBeansManager.java
@@ -9,10 +9,16 @@ import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.Properties;
 
 @Configuration
+@EnableSwagger2
 public class SystemBeansManager {
     @Bean
     public PictureOperations pictureOperations() {
@@ -22,6 +28,15 @@ public class SystemBeansManager {
     @Bean
     public RandomCodeFactory randomCodeFactory() {
         return new RandomCodeFactory();
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
     }
 
     @Bean
